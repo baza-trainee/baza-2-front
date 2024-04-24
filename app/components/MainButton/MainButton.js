b56afccb52253b311522253bd4301a4489cc6da2
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2024-04-23 23:40:44 -0400

    added colors and fonts

---
+++ app/components/MainButton/MainButton.js
@@ -0,0 +1,5 @@
+import styles from "./mainbutton.module.scssmainbutton";
+
+export default function MainButton() {
+  return <></>;
+}
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,4 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import "./styles/global.scss";
 
 export const metadata = {
   title: "Baza trainee 2",
@@ -9,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ua">
+      <body>{children}</body>
     </html>
   );
 }
