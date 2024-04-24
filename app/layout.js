aa3337a5eb6533bae8ec1fd8d75b45dac02ea340
Богдан <sysoevbogdan82@gmail.com>
2024-04-24 15:15:39 +0300

    refactor: moved the сomponents and styles folders to the root folder

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import "./styles/global.scss";
+import "../styles/global.scss";
 
 export const metadata = {
-  title: "Baza trainee 2",
-  description: "Baza trainee web site",
+	title: "Baza trainee 2",
+	description: "Baza trainee web site",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ua">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="ua">
+			<body>{children}</body>
+		</html>
+	);
 }
