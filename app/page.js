0f692c38239047697c601c0d45fe76f0f5a46a5c
artyomakulov <artyomakulov@gmail.com>
2024-04-26 16:03:22 +0300

    add FeedbackCard component
    add FeedbackCard component and styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import FeedbackCard from "./components/FeedbackCard/FeedbackCard";
 import { ScrollToTopBtn } from "./components/ScrollToTopBtn/ScrollToTopBtn";
 
 export default function Home() {
   return (
     <main>
       <ScrollToTopBtn />
+      <FeedbackCard />
     </main>
   );
 }
