20c5128a2189be0e81d033552e5599a71f68f799
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-04-26 11:34:13 +0300

    created structure card

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import { ScrollToTopBtn } from "./components/ScrollToTopBtn/ScrollToTopBtn";
+import History from "./components/History/History";
 
 export default function Home() {
   return (
     <main>
       <ScrollToTopBtn />
+      <History></History>
     </main>
   );
 }
