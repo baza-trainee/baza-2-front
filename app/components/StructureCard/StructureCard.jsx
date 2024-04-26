2fbf4ae4b08a1a6df707d7e96914b4c356fb6824
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-04-26 11:37:30 +0300

    created structure card and fixed folder

--- app/components/StructureCard/StructureCard.jsx
+++ app/components/StructureCard/StructureCard.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from './card.module.scss';
+import styles from './StructureCard.module.scss';
 
 export default function StructureCard({header, text, image}) {
   
