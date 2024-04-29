1326b106051475ae010149db290679c33ed81fbb
artyomakulov <artyomakulov@gmail.com>
2024-04-29 17:48:23 +0300

    add scroll

--- src/components/shared/MainLink/MainLink.jsx
+++ src/components/shared/MainLink/MainLink.jsx
@@ -1,12 +1,22 @@
+"use client";
+
 import { Link } from "@/src/navigation";
 import styles from "./MainLink.module.scss";
 import linkTypes from "./constants";
 
 const MainLink = ({ url, children, type = linkTypes.DEFAULT }) => {
   const linkClass = `${styles.link} ${styles[`link--${type}`]}`;
-
+  const scrollToBottom = (event) => {
+    if (url === "/contacts") {
+      event.preventDefault();
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
-    <Link href={url} className={linkClass}>
+    <Link href={url} className={linkClass} onClick={scrollToBottom}>
       {children}
     </Link>
   );
