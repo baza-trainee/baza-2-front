3b1868f47702bc1691d9229943a22403a7234f49
artyomakulov <artyomakulov@gmail.com>
2024-04-29 13:31:17 +0300

    add Contacts
    add contacts, styles

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -1,33 +1,29 @@
 import NavLinks from "@/src/components/NavLinks/NavLinks";
 import TestExampleLangDropdown from "@/src/components/TestExampleLangDropdown/TestExampleLangDropdown";
+import ContactLinks from "@/src/components/shared/ContactLinks/ContactLinks";
 import "@/src/styles/global.scss";
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 
 export const metadata = {
   title: "Baza trainee 2",
   description: "Baza trainee web site",
 };
 
-
-export default async function LocaleLayout({
-    children,
-    params: {locale}
-  }) {
-  const messages = await getMessages(); 
-    return (
-      <html lang={locale==='ua' ? 'uk': locale}>
-        <body>
-          <NextIntlClientProvider locale={locale}
-          messages={messages}>
-            <div className="wrapper">
-              <NavLinks />
-              <TestExampleLangDropdown/>
-              {children}
-            </div>
-          </NextIntlClientProvider>
-          
-        </body>
-      </html>
-    );
+export default async function LocaleLayout({ children, params: { locale } }) {
+  const messages = await getMessages();
+  return (
+    <html lang={locale === "ua" ? "uk" : locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <div className="wrapper">
+            <NavLinks />
+            <TestExampleLangDropdown />
+            {children}
+          </div>
+        </NextIntlClientProvider>
+        <ContactLinks />
+      </body>
+    </html>
+  );
 }
