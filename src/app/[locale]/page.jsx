b86a004c2faa504a2d60666d6b9d6c1a56570502
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-04-30 00:56:34 -0400

    added uikit route and transition vars (#15)
    Co-authored-by: Mykhailo Fomenko <85066667+MishaFomenko@users.noreply.github.com>

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,21 +1,11 @@
-import style from "./page.module.scss";
-
 import CarouselTestSection from "@/src/components/CarouselTestSection/CarouselTestSection";
 import RoleCardTestSection from "@/src/components/RoleCardTestSection/RoleCardTestSection";
-import ExampleButtons from "@/src/components/ExampleButtons/ExampleButtons";
-import { ScrollToTopBtn } from "@/src/components/shared/ScrollToTopBtn/ScrollToTopBtn";
-import FeedbackCard from "@/src/components/shared/FeedbackCard/FeedbackCard";
-import FooterLinks from "@/src/components/FooterLinks/FooterLinks";
 
 export default function Home() {
   return (
     <main>
-      <ExampleButtons />
-      <ScrollToTopBtn />
-      <FeedbackCard />
       <CarouselTestSection />
       <RoleCardTestSection />
-      <FooterLinks />
     </main>
   );
 }
