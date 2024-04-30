cc4353c1a448d897a9ff78829687a1610d21dc4d
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2024-04-30 07:12:25 +0300

    Fix/links (#13)
    * Fix links
    
    add types of link, add styles
    
    * add scroll
    
    * fix
    
    * fix conflick
    
    * fix styles
    
    * fix styles
    
    add focus

--- src/components/FooterLinks/constants.js
+++ src/components/FooterLinks/constants.js
@@ -1,6 +1,15 @@
+import linkTypes from "../shared/MainLink/constants";
+
 export const navigationLinks = [
-  { name: "Головна", url: "/main" },
+  { name: "Головна", url: "/" },
   { name: "Стажування", url: "/internship" },
   { name: "Проєкти", url: "/projects" },
   { name: "Блог", url: "/blog" },
+];
+
+export const helpLinks = [
+  { name: "Політика конфіденційності", url: "/", type: linkTypes.HELP },
+  { name: "Правила користування сайтом", url: "/", type: linkTypes.HELP },
+  { name: "Статут", url: "/", type: linkTypes.DEFAULT },
+  { name: "Звітність", url: "/", type: linkTypes.DEFAULT },
 ];
