7f5135ea875feba40390fcda84d26928cc481666
Богдан <sysoevbogdan82@gmail.com>
2024-05-01 13:39:08 +0300

    refactor: created Icon component
    created Icon component, added new webpack plugin. Minor fixes for LangDropDown component.

--- src/components/LangDropdown/LangDropdown.jsx
+++ src/components/LangDropdown/LangDropdown.jsx
@@ -1,82 +1,58 @@
-'use client';
-import style from './LangDropdown.module.scss';
-import { useEffect, useRef, useState } from 'react';
-import { useLocale } from 'next-intl';
-import { locales } from '@/src/i18n';
-import { usePathname, useRouter } from '@/src/navigation';
-import HeaderCaretDown from '../icons/HeaderArowDown';
+"use client"
+import style from "./LangDropdown.module.scss"
+import { useEffect, useRef, useState } from "react"
+import { useLocale } from "next-intl"
+import { locales } from "@/src/i18n"
+import { usePathname, useRouter } from "@/src/navigation"
+import { Icon } from "../shared/Icon/Icon"
+import clsx from "clsx"
 
-export default function LangDropdown (){
-  const router = useRouter();
-  const path = usePathname();
-  const locale = useLocale();
-  const [currentLocale, setCurrentLocale] = useState(locale);
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef(null);
-  const submenuRef = useRef(null);
+export default function LangDropdown() {
+  const router = useRouter()
+  const path = usePathname()
+  const locale = useLocale()
+  const [currentLocale, setCurrentLocale] = useState(locale)
+  const [isOpen, setIsOpen] = useState(false)
+  const menuRef = useRef(null)
+  const submenuRef = useRef(null)
 
-  const handleCheckLocale = (item) => {
-    setIsOpen(!isOpen);
-    setCurrentLocale(item);
-    router.replace(path, { locale: item });
-  };
+  const handleCheckLocale = item => {
+    setIsOpen(!isOpen)
+    setCurrentLocale(item)
+    router.replace(path, { locale: item })
+  }
 
-  const handleOutsideClick = (event) => {
+  const handleOutsideClick = event => {
     if (
-      !submenuRef.current?.contains(
-        event.target
-      ) &&
-      !(
-        event.target === menuRef.current ||
-        menuRef.current?.contains(
-          event.target
-        )
-      )
+      !submenuRef.current?.contains(event.target) &&
+      !(event.target === menuRef.current || menuRef.current?.contains(event.target))
     ) {
-      setIsOpen(false);
+      setIsOpen(false)
     }
-  };
+  }
   useEffect(() => {
-    window.addEventListener('click', handleOutsideClick);
-    return () =>
-      window.removeEventListener(
-        'click',
-        handleOutsideClick
-      );
-  }, [isOpen]);
+    window.addEventListener("click", handleOutsideClick)
+    return () => window.removeEventListener("click", handleOutsideClick)
+  }, [isOpen])
   return (
-    <div
-    className={style.box}
-    >
-      <div
-        ref={menuRef}
-        onClick={() => setIsOpen(!isOpen)}
-        className={style.btn_lang}
-      >
-        <span>
-        {currentLocale.toUpperCase()}
-        </span>
-        <span className={isOpen ? style.btn_icon_up:style.btn_icon}>
-          <HeaderCaretDown/>
+    <div className={style.box}>
+      <div ref={menuRef} onClick={() => setIsOpen(!isOpen)} className={style.btn_lang}>
+        <span>{currentLocale.toUpperCase()}</span>
+        <span className={clsx(style.btn_icon, isOpen && style.btn_icon_up)}>
+          <Icon name="intl-select-arrow" />
         </span>
       </div>
       {isOpen && (
-        <div
-          ref={submenuRef}
-          className={style.options}
-        >
-          {locales.map((item) => (
-            item !==currentLocale ?
-            <span
-              key={item}
-              onClick={() => handleCheckLocale(item)}
-            >
-              {item.toUpperCase()}
-            </span>:
-            null
-          ))}
+        <div ref={submenuRef} className={style.options}>
+          {locales.map(item =>
+            item !== currentLocale ? (
+              <span key={item} onClick={() => handleCheckLocale(item)}>
+                {item.toUpperCase()}
+              </span>
+            ) : null
+          )}
         </div>
       )}
     </div>
-  );
-};
+  )
+}
