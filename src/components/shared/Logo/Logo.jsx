3a063871cf9dcde1cdd898a58f8b8e2467025141
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-01 12:58:15 +0300

    fix: change logic Logo component

--- src/components/shared/Logo/Logo.jsx
+++ src/components/shared/Logo/Logo.jsx
@@ -2,22 +2,32 @@
 import styles from './Logo.module.scss';
 import Image from 'next/image';
 import { Link } from '@/src/navigation';
+// variant header або footer
+const logoConfig = {
+  HEADER:'header',
+  FOOTER: "footer",
+}
 
-export default function Logo({variant='header'}) {
-  const logoClass = `${styles[`logo_${variant}`]}`;
+export default function Logo({variant=logoConfig.HEADER}) {
+  const logoClass = `${styles[
+    variant === logoConfig.HEADER || 
+    variant === logoConfig.FOOTER ? 
+    variant : 
+    logoConfig.HEADER]}`;
 
   const scrollToTop = (event) => {
-    if (variant === 'footer') {
+    if (variant === logoConfig.FOOTER) {
       event.preventDefault();
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-      });
     }
+    window.scrollTo({
+      top: 0,
+      behavior: variant === logoConfig.HEADER ? 'instant' : "smooth"
+  });
   }
 
-  return <Link href={'/'} className={logoClass} onClick={scrollToTop}>
-    <Image src="/images/main_logo.svg" alt="Logo" fill={true}/>
-  </Link>
-    
+  return (
+    <Link href={'/'} className={logoClass} onClick={scrollToTop}>
+      <Image src="/images/main_logo.svg" alt="Logo" fill={true}/>
+    </Link>
+  )
 }
