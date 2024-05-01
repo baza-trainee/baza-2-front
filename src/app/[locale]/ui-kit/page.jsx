93e8d7599ef097fa59f1ea8953abcc0aac20f638
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-05-01 06:55:20 +0300

    Feature/partner card (#21)
    * styles changed in Structure Card
    
    * styles #2  changed in Structure Card
    
    * Fixed styles
    
    * create PartnerCard component
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/app/[locale]/ui-kit/page.jsx
+++ src/app/[locale]/ui-kit/page.jsx
@@ -2,6 +2,8 @@ import ExampleButtons from "@/src/components/ExampleButtons/ExampleButtons";
 import { ScrollToTopBtn } from "@/src/components/shared/ScrollToTopBtn/ScrollToTopBtn";
 import FeedbackCard from "@/src/components/shared/FeedbackCard/FeedbackCard";
 import FooterLinks from "@/src/components/FooterLinks/FooterLinks";
+import StructureCard from "@/src/components/shared/StructureCard/StructureCard";
+import PartnerCard from "@/src/components/shared/PartnerCard/PartnerCard";
 import Logo from "@/src/components/shared/Logo/Logo";
 
 export default function UiKit() {
@@ -11,6 +13,8 @@ export default function UiKit() {
       <ScrollToTopBtn />
       <FeedbackCard />
       <FooterLinks />
+      <StructureCard />
+      <PartnerCard />
       <Logo variant="footer"/>
     </main>
   );
