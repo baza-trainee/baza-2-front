ee9159658b90af6cefa9324fccc11b50bec725f1
artyomakulov <artyomakulov@gmail.com>
2024-05-02 14:33:39 +0300

    merge main branch

--- src/components/shared/SocialIcons/SocialIcons.jsx
+++ src/components/shared/SocialIcons/SocialIcons.jsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { Icon } from "../Icon/Icon";
+import styles from "./SocialIcons.module.scss";
+
 const SocialIcons = () => {
-  return <div>SocialIcons</div>;
+  return (
+    <div>
+      <a href="https://www.linkedin.com/company/baza-trainee-ukraine/">
+        <Icon name="linkedin" className={styles.icon} />
+      </a>
+      <a href="https://www.facebook.com/BazaIT/">
+        <Icon name="facebook" className={styles.icon} />
+      </a>
+      <a href="https://www.facebook.com/BazaIT/" className={styles.link}>
+        <Icon name="telegram" className={styles.icon} />
+      </a>
+    </div>
+  );
 };
 
 export default SocialIcons;
