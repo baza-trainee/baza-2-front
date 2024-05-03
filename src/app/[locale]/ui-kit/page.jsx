a651eb1ef4dd3df4eac64208cc55984c6ac869b2
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2024-05-03 05:40:03 +0300

    Feature/social icons (#33)
    * create SocialIcon comp
    
    * merge main branch
    
    * add styles to Social Links component
    
    * fix link

--- src/app/[locale]/ui-kit/page.jsx
+++ src/app/[locale]/ui-kit/page.jsx
@@ -5,6 +5,7 @@ import FooterLinks from "@/src/components/FooterLinks/FooterLinks";
 import StructureCard from "@/src/components/shared/StructureCard/StructureCard";
 import PartnerCard from "@/src/components/shared/PartnerCard/PartnerCard";
 import Logo from "@/src/components/shared/Logo/Logo";
+import SocialIcons from "@/src/components/shared/SocialIcons/SocialIcons";
 
 export default function UiKit() {
   return (
@@ -16,6 +17,7 @@ export default function UiKit() {
       <StructureCard />
       <PartnerCard />
       <Logo variant="footer" />
+      <SocialIcons  />
     </main>
   );
 }
