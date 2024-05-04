6a88079eaac1536cf8e7c8591fce00fc32182b1b
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-04 10:25:09 +0300

    add react-hook-form

--- src/components/forms/ContactForm/ContactForm.jsx
+++ src/components/forms/ContactForm/ContactForm.jsx
@@ -1,15 +1,30 @@
+"use client";
 import styles from "./ContactForm.module.scss";
+import { useForm } from "react-hook-form"
 import { useTranslations } from "next-intl";
 import MainButton from "../../shared/MainButton/MainButton";
 
 export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data)
+
+
   const t = useTranslations("Main.feedback_form");
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label>name</label>
         <input type="text"/>
       </div>
+      <input {...register('firstName')} />
+      <input {...register('lastName', { required: true })} />
+      {errors.lastName && <p>Last name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} />
+      {errors.age && <p>Please enter number for age.</p>}
       <MainButton type="submit">{t("btn_send")}</MainButton>
     </form>
   )
