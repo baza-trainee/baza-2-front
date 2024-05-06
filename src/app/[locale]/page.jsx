c38afd32063f7f9eb691f76ce80a64e543e5f6f7
rodindima <rodindima1984@gmail.com>
2024-05-06 13:57:51 +0300

    feat: add new Mentor section component

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,8 +1,10 @@
 import CarouselTestSection from "@/src/components/CarouselTestSection/CarouselTestSection";
 import Counter from "@/src/components/Counter/Counter";
 import FAQ from "@/src/components/FAQ/FAQ";
+import HistorySection from "@/src/components/HistorySection/HistorySection";
+import MentorSection from "@/src/components/MentorSection/MentorSection";
 import RoleCardTestSection from "@/src/components/RoleCardTestSection/RoleCardTestSection";
-import Timeline from "@/src/components/Timeline/Timeline";
+
 
 export default function Home() {
   return (
@@ -10,8 +12,9 @@ export default function Home() {
       <input placeholder="" />
       <CarouselTestSection />
       <RoleCardTestSection />
+      <HistorySection />
       <Counter />
-      <Timeline />
+      <MentorSection />
       <FAQ />
     </main>
   );
