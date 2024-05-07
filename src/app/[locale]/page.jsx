2eb2fea9e58b85464ab3b89e8228c5e4953c56a6
Rodin-do-it <133952682+RodinDima@users.noreply.github.com>
2024-05-07 03:59:45 +0300

    feat: add new Mentor section component (#39)
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -2,6 +2,8 @@ import { ArticlesSection } from "@/src/components/ArticlesSection/ArticlesSectio
 import CarouselTestSection from "@/src/components/CarouselTestSection/CarouselTestSection";
 import Counter from "@/src/components/Counter/Counter";
 import FAQ from "@/src/components/FAQ/FAQ";
+import HistorySection from "@/src/components/HistorySection/HistorySection";
+import MentorSection from "@/src/components/MentorSection/MentorSection";
 import RoleCardTestSection from "@/src/components/RoleCardTestSection/RoleCardTestSection";
 import Timeline from "@/src/components/Timeline/Timeline";
 import { ScrollToTopBtn } from "@/src/components/shared/ScrollToTopBtn/ScrollToTopBtn";
@@ -12,7 +14,9 @@ export default function Home() {
       <input placeholder="" />
       <CarouselTestSection />
       <RoleCardTestSection />
+      <HistorySection />
       <Counter />
+      <MentorSection />
       <Timeline />
       <ArticlesSection />
       <ScrollToTopBtn />
