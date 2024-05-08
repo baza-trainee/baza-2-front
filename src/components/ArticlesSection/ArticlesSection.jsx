732b41e4cf9f4b25844acaebac8ff10f73cbe458
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-05-08 14:55:59 +0200

    add styles for swiper, add svg for btn scroll to top

--- src/components/ArticlesSection/ArticlesSection.jsx
+++ src/components/ArticlesSection/ArticlesSection.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import styles from "./ArticlesSection.module.scss";
 import { ArticleCard } from "../shared/ArticleCard/ArticleCard";
 import CarouselButton from "../shared/Carousel/CarouselButton/CarouselButton";
 import Carousel from "../shared/Carousel/Carousel";
@@ -9,28 +8,41 @@ import { Navigation } from "swiper/modules";
 import clsx from "clsx";
 import { items } from "./items";
 
+import styles from "./ArticlesSection.module.scss";
+
 export const ArticlesSection = () => {
   const t = useTranslations("Main.press_about_section");
 
   return (
-    <section className="articles-container">
+    <section className={styles.articlesContainer}>
       <div className={styles.wrapperAll}>
         <h2 className={styles.title}>{t("title")}</h2>
-        <div className={styles.navigation}>
-          <CarouselButton className={clsx("prevEl", styles.prevEl)} />
-          <CarouselButton className="nextEl" />
+        <div className={styles.navigationButtons}>
+          <CarouselButton
+            className={clsx("prevElArticle", styles.prevElArticle)}
+          />
+          <CarouselButton className="nextElArticle" />
         </div>
-        <Carousel
-          modules={[Navigation]}
-          paginationEl={".custom-pagination"}
-          prevEl={".prevEl"}
-          nextEl={".nextEl"}
-          className={styles.wrapper}
-          slidesPerView={3}
-          items={items}
-          renderItem={(item) => <ArticleCard key={Date.now()} item={item} />}
-        />
       </div>
+
+      <Carousel
+        className={clsx("swiper", styles.mySwiper)}
+        slideClassName={(clsx("swiper-slide"), styles.mySwiperItem)}
+        modules={[Navigation]}
+        prevEl={".prevElArticle"}
+        nextEl={".nextElArticle"}
+        slidesPerView={1}
+        breakpoints={{
+          1280: {
+            slidesPerView: 3,
+          },
+          1920: {
+            slidesPerView: 4,
+          },
+        }}
+        items={items}
+        renderItem={(item) => <ArticleCard key={Date.now()} item={item} />}
+      />
     </section>
   );
 };
