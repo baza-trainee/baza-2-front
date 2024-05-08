3dd948c3becae529b47fdf4de5fbb415394ec3d5
artyomakulov <artyomakulov@gmail.com>
2024-05-08 20:29:16 +0300

    footer translation
    add footer translation
    fix linter error in FeedbackCard styles

--- src/components/shared/FooterLinks/constants.js
+++ src/components/shared/FooterLinks/constants.js
@@ -1,15 +1,15 @@
 import linkTypes from "../MainLink/constants";
 
 export const navigationLinks = [
-  { name: "Головна", url: "/", type: linkTypes.FOOTER },
-  { name: "Стажування", url: "/internship", type: linkTypes.FOOTER },
-  { name: "Проєкти", url: "/projects", type: linkTypes.FOOTER },
-  { name: "Блог", url: "/blog", type: linkTypes.FOOTER },
+  { name: "main", url: "/", type: linkTypes.FOOTER },
+  { name: "internship", url: "/internship", type: linkTypes.FOOTER },
+  { name: "projects", url: "/projects", type: linkTypes.FOOTER },
+  { name: "blog", url: "/blog", type: linkTypes.FOOTER },
 ];
 
 export const helpLinks = [
-  { name: "Політика конфіденційності", url: "/", type: linkTypes.HELP },
-  { name: "Правила користування сайтом", url: "/", type: linkTypes.HELP },
-  { name: "Статут", url: "/", type: linkTypes.FOOTER },
-  { name: "Звітність", url: "/", type: linkTypes.FOOTER },
+  { name: "privacy_policy", url: "/", type: linkTypes.HELP },
+  { name: "site_use_policy", url: "/", type: linkTypes.HELP },
+  { name: "regulations", url: "/", type: linkTypes.FOOTER },
+  { name: "reporting", url: "/", type: linkTypes.FOOTER },
 ];
