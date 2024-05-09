9776dc22cd57cd69759e0eb8dc96118352cb533b
Anastasiia <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-05-09 05:08:11 +0200

    Feature/refactor articles section (#52)
    * add styles for swiper, add svg for btn scroll to top
    
    * rewrite property for swiper
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/components/shared/ScrollToTopBtn/ScrollToTopBtn.jsx
+++ src/components/shared/ScrollToTopBtn/ScrollToTopBtn.jsx
@@ -40,12 +40,35 @@ export const ScrollToTopBtn = () => {
         }
         type="button"
       >
-        <Icon
-          name="scroll-top-arrow"
-          width={25}
-          height={29}
-          alt="button to top"
-        />
+        <svg
+          width="25"
+          height="29"
+          viewBox="0 0 25 29"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M0.0359831 12.4994L11.7032 0.402191L12.0648 0.0273333L12.4246 0.403834L24.0366 12.554L23.3136 13.2449L12.5605 1.99343L12.5012 28.0288L11.5012 28.0265L11.5605 1.99065L0.755765 13.1936L0.0359831 12.4994Z"
+            fill="url(#paint0_linear_2137_6054)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_2137_6054"
+              x1="0.0644531"
+              y1="0"
+              x2="2.03443"
+              y2="34.9901"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#E37CC1" />
+              <stop offset="0.365" stopColor="#E231A8" />
+              <stop offset="0.76" stopColor="#9D2F79" />
+              <stop offset="1" stopColor="#851A62" />
+            </linearGradient>
+          </defs>
+        </svg>
       </button>
     </>
   );
