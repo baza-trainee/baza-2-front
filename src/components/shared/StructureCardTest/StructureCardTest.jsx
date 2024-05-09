2d60a7b249b95044dcec36b93a16f42b5327a575
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-05-09 19:53:07 +0300

    deleted extra imports

--- src/components/shared/StructureCardTest/StructureCardTest.jsx
+++ src/components/shared/StructureCardTest/StructureCardTest.jsx
@@ -1,10 +1,5 @@
 import styles from "./StructureCardTest.module.scss";
-import Image from "next/image";
-// import educat from './images/educat.svg';
-// import skill from './images/skill.svg';
-// import logoblack from './images/logoblack.svg';
 import { useTranslations } from "next-intl";
-
 
 export default function StructureCardTest({ item }) {
   const t = useTranslations("Main.our_structure_section");
@@ -23,38 +18,3 @@ export default function StructureCardTest({ item }) {
       </div>
   );
 }
-
-      {/* <div className={styles.cardStyle}>
-        <div className={styles.cardWrapper}>
-        <Image src={skill} alt="skill logo" />
-        </div>
-        <div className={styles.textCard}>
-          <p className={styles.header}>{t(title)}</p>
-          <p className={styles.text}>
-          {t(text)}
-          </p>
-        </div>
-      </div>
-      <div className={styles.cardStyle}>
-        <div className={styles.eduCardWrapper}>
-          <div className={styles.ellipseWrapper}></div>
-          <Image className={styles.svgWrapper} src={educat} alt="educate logo" />
-        </div>
-        <div className={styles.textCard}>
-          <p className={styles.header}>{t(title)}</p>
-          <p className={styles.text}>
-          {t(text)}
-          </p>
-        </div>
-      </div>
-      <div className={styles.cardStyle}>
-        <div className={styles.cardWrapper}>
-        <Image  className={styles.svgWrapper} src={logoblack} alt="edu logo svg" />
-        </div>
-        <div className={styles.textCard}>
-          <p className={styles.header}>{t(title)}</p>
-          <p className={styles.text}>
-          {t(text)}
-          </p>
-        </div>
-      </div> */}
