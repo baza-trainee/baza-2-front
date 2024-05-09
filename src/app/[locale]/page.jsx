dc601d3b0a320f753d3a11cf04ecdcb74560a7ed
Mykhailo Fomenko <85066667+MishaFomenko@users.noreply.github.com>
2024-05-09 13:01:07 -0400

    fixed main page imports

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,5 +1,5 @@
 import { ArticlesSection } from "@/src/components/ArticlesSection/ArticlesSection";
-//import CarouselTestSection from "@/src/components/CarouselTestSection/CarouselTestSection";
+// import CarouselTestSection from "@/src/components/CarouselTestSection/CarouselTestSection";
 import ContactFormSection from "@/src/components/ContactFormSection/ContactFormSection";
 import Counter from "@/src/components/Counter/Counter";
 import FAQ from "@/src/components/FAQ/FAQ";
@@ -7,16 +7,15 @@ import ReviewsSection from "@/src/components/ReviewsSection/ReviewsSection";
 import HistorySection from "@/src/components/HistorySection/HistorySection";
 import MentorSection from "@/src/components/MentorSection/MentorSection";
 import HeroSection from "@/src/components/HeroSection/HeroSection";
-//import RoleCardTestSection from "@/src/components/RoleCardTestSection/RoleCardTestSection";
-//import Timeline from "@/src/components/Timeline/Timeline";
+// import RoleCardTestSection from "@/src/components/RoleCardTestSection/RoleCardTestSection";
+import Timeline from "@/src/components/Timeline/Timeline";
 import { ScrollToTopBtn } from "@/src/components/shared/ScrollToTopBtn/ScrollToTopBtn";
 import StructureSectionTest from "@/src/components/StructureSectionTest/StructureSectionTest";
-
 
 export default function Home() {
   return (
     <main>
-      <HeroSection/>
+      <HeroSection />
       {/* <input placeholder="" /> */}
       {/* <CarouselTestSection /> */}
       {/* <RoleCardTestSection /> */}
