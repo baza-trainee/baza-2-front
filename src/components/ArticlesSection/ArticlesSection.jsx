7e642a886cae55bad837dab25246c5fe954c2d9c
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-05-12 22:58:27 +0200

    fix adaptive for mobile

--- src/components/ArticlesSection/ArticlesSection.jsx
+++ src/components/ArticlesSection/ArticlesSection.jsx
@@ -4,12 +4,13 @@ import { useTranslations } from "next-intl";
 import { ArticleCard } from "../shared/ArticleCard/ArticleCard";
 import CarouselButton from "../shared/Carousel/CarouselButton/CarouselButton";
 import Carousel from "../shared/Carousel/Carousel";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import clsx from "clsx";
 import { items } from "./items";
 
 import styles from "./ArticlesSection.module.scss";
 import { createKey } from "@/src/lib/utils/createKey";
+import CarouselPagination from "../shared/Carousel/CarouselPagination/CarouselPagination";
 
 export const ArticlesSection = () => {
   const t = useTranslations("Main.press_about_section");
@@ -25,25 +26,39 @@ export const ArticlesSection = () => {
           <CarouselButton className="nextElArticle" />
         </div>
       </div>
-
       <Carousel
         slideClassName={clsx("swiper-slide", styles.mySwiperItem)}
-        modules={[Navigation]}
+        modules={[Navigation, Pagination]}
+        paginationEl={".custom-pagination-article"}
         prevEl={".prevElArticle"}
         nextEl={".nextElArticle"}
+        speed={1000}
+        pagination={{
+          clickable: true,
+        }}
+        centeredSlides={true}
         slidesPerView={1}
         spaceBetween={24}
         breakpoints={{
           1280: {
             slidesPerView: 2,
+            pagination: {
+              clickable: false,
+            },
           },
+          // 1440: {
+          //   spaceBetween: 30,
+          // },
           1920: {
             spaceBetween: 30,
-            slidesPerView: 4,
+            slidesPerView: 3,
           },
         }}
         items={items}
         renderItem={(item) => <ArticleCard key={createKey()} item={item} />}
+      />
+      <CarouselPagination
+        className={clsx("custom-pagination-article", styles.paginationArticle)}
       />
     </section>
   );
