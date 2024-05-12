5e26bafd1b17ff1914710ccfe5fc17e41449fede
Mykhailo Fomenko <85066667+MishaFomenko@users.noreply.github.com>
2024-05-12 12:05:02 -0400

    fixed keys in structure section

--- src/components/StructureSection/StructureSection.jsx
+++ src/components/StructureSection/StructureSection.jsx
@@ -4,6 +4,7 @@ import { useTranslations } from "next-intl";
 import StructureCard from "../shared/StructureCard/StructureCard";
 import styles from "./StructureSection.module.scss";
 import { items } from "./items";
+import { createKey } from "../../lib/utils/createKey";
 
 export default function StructureSection() {
   const t = useTranslations("Main.our_structure_section");
@@ -14,9 +15,10 @@ export default function StructureSection() {
         <p>{t("title")}</p>
       </div>
       <div className={styles.cardContainer}>
-        {items.map((item) => (
-          <StructureCard key={Date.now()} item={item} />
-        ))}
+        {items.map((item) => {
+          const cardKey = createKey();
+          return <StructureCard key={cardKey} item={item} />;
+        })}
       </div>
     </section>
   );
