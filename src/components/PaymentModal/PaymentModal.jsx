2d8f0aa43cf6fa490081857e49ff1e6bd5411198
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-15 16:08:51 +0300

    add ariaLabel close btn

--- src/components/PaymentModal/PaymentModal.jsx
+++ src/components/PaymentModal/PaymentModal.jsx
@@ -1,58 +1,54 @@
 "use client";
-
+import { useState } from 'react';
+import { useTranslations } from "next-intl";
+import { useParams } from 'next/navigation';
+import { useRouter } from '@/src/navigation';
+import { createKey } from '@/src/lib/utils/createKey';
 import { Icon } from '../shared/Icon/Icon';
 import MainButton from '../shared/MainButton/MainButton';
-import { useTranslations } from "next-intl";
-import styles from './PaymentModal.module.scss';
-import clsx from "clsx";
-import { useState } from 'react';
-import { createKey } from '@/src/lib/utils/createKey';
-import { btnItems } from './btnItems';
 import Modal from '../shared/Modal/Modal';
 import stateModalPayment from '@/src/state/stateModalPayment';
 import usePaymentHandler from './usePaymentHandler';
-import { useRouter } from '@/src/navigation';
-import { useParams } from 'next/navigation';
+import { btnItems } from './btnItems';
+import styles from './PaymentModal.module.scss';
+import clsx from "clsx";
 
 export default function PaymentModal() {
+  // Отримуємо стан.
   const isOpen = stateModalPayment(state => state.isOpen);
   const close = stateModalPayment(state => state.close);
-
+  // контент.
   const router = useRouter();
-  const params = useParams();
-
+  const { locale } = useParams();
+  // контент.
   const t = useTranslations("Modal");
-
+  // локальний стан.
   const [amount, setAmount] = useState('0');
   const [thank, setThank] = useState(false);
 
-
-
   const submit = () => {
-    console.log(usePaymentHandler(amount, params.locale))  
+    console.log(usePaymentHandler(amount, locale))  
     setThank(true)
     setAmount('0')
   }
 
   const handleClose = () => {
-    setAmount('0')
-    close()
-  }
-  
-  const home = () =>{
-    router.push('/')
     setThank(false)
     setAmount('0')
     close()
   }
+  
+  const goHome = () => {
+    router.push('/')
+    handleClose()
+  }
 
   return <Modal isOpen={isOpen} handleClose={handleClose}>
     <div className={styles.card}>
-
       {thank?
       <>
         <h2>{t('thanks_support')}</h2>
-        <MainButton onClick={home}>{t('btn_home')}</MainButton>
+        <MainButton onClick={goHome}>{t('btn_home')}</MainButton>
       </>:
       <>
         <p>{t('amount')} <Icon className={styles.icon} name='donate-modal-icon'/> </p>
@@ -72,7 +68,14 @@ export default function PaymentModal() {
         <MainButton disabled={amount==='0'} onClick={submit}>{t('btn_support')}</MainButton>
       </>
       }
-      <button type='button' aria-label='Close modal' onClick={handleClose} className={styles.close_btn}><Icon name='close'/></button>
+
+      <button type='button' 
+        aria-label={thank ? 
+          t('ariaLabel_btn_close_2') : 
+          t('ariaLabel_btn_close_1')} 
+        onClick={handleClose} 
+        className={styles.close_btn}><Icon name='close'/>
+      </button>
     </div>
   </Modal>
 }
