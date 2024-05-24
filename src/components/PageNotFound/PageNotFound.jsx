87e5a893c6c6317a09707f493f595eaa6a84e3a9
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-24 16:43:36 +0300

    fix Router PageNotFound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,14 +1,25 @@
-import Link from "next/link";
+"use client";
 import styles from "./PageNotFound.module.scss";
+import { useCallback } from "react";
+import { useRouter } from "@/src/navigation";
+import MainButton from "../shared/MainButton/MainButton";
 
 export const PageNotFound = ({ textErr, textErrBtn }) => {
+
+  const router = useRouter();
+  
+  const handleGoHome = useCallback(() => {
+    router.push('/')
+  }, []); 
+
   return (
     <main className={styles.mainErr}>
       <h2>404</h2>
       <p>{textErr}</p>
-      <Link className={styles.btnBack} href="/">
-        {textErrBtn}
-      </Link>
+      <MainButton
+        onClick={handleGoHome}>
+          {textErrBtn}
+      </MainButton>
     </main>
   );
 };
