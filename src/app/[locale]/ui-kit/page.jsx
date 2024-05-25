0e8f1a16ff868058f4761bdf9814d33a956d580d
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-25 09:02:19 +0300

    adaptive error page

--- src/app/[locale]/ui-kit/page.jsx
+++ src/app/[locale]/ui-kit/page.jsx
@@ -4,18 +4,14 @@ import PartnerCard from "@/src/components/shared/PartnerCard/PartnerCard";
 import SocialIcons from "@/src/components/shared/SocialIcons/SocialIcons";
 import HeroCard from "@/src/components/shared/HeroCard/HeroCard";
 import StructureSection from "../../../components/StructureSection/StructureSection";
-import PaymentModal from "@/src/components/PaymentModal/PaymentModal";
-import ControlBtnModalPayment from "@/src/components/shared/controlButtons/ControlBtnModalPayment";
 import RegistrationFormModal from "@/src/components/RegistrationFormModal/RegistrationFormModal";
 import ControlBtnRegistrationFormModal from "@/src/components/shared/controlButtons/ControlBtnRegistrationFormModal";
 
 export default function UiKit() {
   return (
     <main>
-      <PaymentModal />
       <ScrollToTopBtn />
       <FooterLinks />
-      <ControlBtnModalPayment>Test PaymentModal</ControlBtnModalPayment>
       <ControlBtnRegistrationFormModal>Test RegistrationFormModal</ControlBtnRegistrationFormModal>
       <PartnerCard />
       <StructureSection />
