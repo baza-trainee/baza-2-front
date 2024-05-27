5c52ac2865691a582b3e3bd36e17c57cea3ab465
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-27 17:17:22 +0300

    fix PaymentModal

--- src/components/PaymentModal/FormPayment/FormPayment.jsx
+++ src/components/PaymentModal/FormPayment/FormPayment.jsx
@@ -84,22 +84,20 @@ export default function FormPayment({handleThank}) {
     }}>
     <p>{t('amount')} <Icon className={styles.icon} name='donate-modal-icon'/></p>
     <div className={clsx(styles.amount, amount.length > 1 && !readOnly && styles._active)}>
+    
       <input 
-        id='amount'
         value={amount} 
         type = "text"
         inputMode="numeric"
         className={styles.input}
         ref={inputRef}
-        size={ amount.length < 1 ? 1 : amount.length } 
+         size={4}
         onInput={(e)=>{isValidate(e.target.value)}}
         onBlur={(e)=>{fixedAmount(e)}}
         readOnly={readOnly}
         autoComplete="off"
       />
-
-      <label htmlFor='amount'><Icon name='currency'/></label>
-
+      <div className={styles.label}><span>{amount}</span><Icon name='currency'/></div>
       {error && <p
         className={clsx(styles.error, error?.length > 2 && styles._message)}>{error}</p>
       }
