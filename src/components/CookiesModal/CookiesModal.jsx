d65f5e1cd288e00ca58a05ae25539fc40a17f03b
Brodich Vlad <brodich_vlad@ukr.net>
2024-05-27 11:58:07 +0300

    add comment

--- src/components/CookiesModal/CookiesModal.jsx
+++ src/components/CookiesModal/CookiesModal.jsx
@@ -7,7 +7,7 @@ import MainButton from '../shared/MainButton/MainButton';
 import styles from './CookiesModal.module.scss';
 import CloseBtn from '../shared/CloseBtn/CloseBtn';
 
-// Examples Cookies:
+// Examples Cookies:  expires: 365 днів.
 // Cookies.set('name', 'value', { expires: 365 })
 // Cookies.get('name') // => 'value'
 // Cookies.remove('name')
@@ -15,15 +15,15 @@ import CloseBtn from '../shared/CloseBtn/CloseBtn';
 export default function CookiesModal() {
   // контент.
   const t = useTranslations("cookies");
-// локальний стан.;           
+  // локальний стан.          
   const [showModal, setShowModal] = useState(false);
-
+  // перевіряємо чи cookiesAccepted.
   useEffect(() => {
     if (!Cookies.get('cookiesAccepted')) {
       setShowModal(true);
     }
   }, []);
-
+  // зберігаємо cookiesAccepted зі значенням true.
   const acceptCookies = () => {
     Cookies.set('cookiesAccepted', 'true', {
       expires: 1 / 48,
@@ -34,9 +34,9 @@ export default function CookiesModal() {
 
   const closeModal=()=>{
     setShowModal(false)
-  }
+  };
 
-  if(!showModal){return null}
+  if(!showModal){return null};
 
   return <div className={styles.wrapper}>
     <div className={styles.modal}>
@@ -49,6 +49,7 @@ export default function CookiesModal() {
         rel="noreferrer noopener"
         >{t('privacy_policy')}</a>
       </p>
+
       <MainButton className={styles.btn} 
         onClick={acceptCookies}>Ok</MainButton>
 
