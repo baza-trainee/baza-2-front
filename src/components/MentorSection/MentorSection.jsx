4c9353d650f19ffd3969de34f813699512644ae4
Богдан <sysoevbogdan82@gmail.com>
2024-05-31 15:16:13 +0300

    refactor/ updated HistorySection styles

--- src/components/MentorSection/MentorSection.jsx
+++ src/components/MentorSection/MentorSection.jsx
@@ -10,24 +10,18 @@ const MentorSection = () => {
     <section className={styles.mentor}>
       <div className={styles.container}>
         <h2 className={styles.title}>{t("title")}</h2>
-        
+
         <div className={styles.wrapper}>
           <div className={styles.text}>
-            <p className={styles.text_mb}>
-              {t("paragraph_1")}
-            </p>
-            <p className={styles.text_mb}>
-              {t("paragraph_2")}
-            </p>
-            <p>
-              {t("paragraph_3")}
-            </p>
+            <p className={styles.text_mb}>{t("paragraph_1")}</p>
+            <p className={styles.text_mb}>{t("paragraph_2")}</p>
+            <p>{t("paragraph_3")}</p>
           </div>
 
           <div className={styles.wrapper_img}>
             <Image
               className={styles.image_mobile}
-              src={'/images/mentor_section/mentor_mobile.png'}
+              src={"/images/mentor_section/mentor_mobile.png"}
               fill
               sizes="100%"
               alt="About mentor team"
@@ -35,7 +29,7 @@ const MentorSection = () => {
             />
             <Image
               className={styles.image}
-              src={'/images/mentor_section/mentor.png'}
+              src={"/images/mentor_section/mentor.png"}
               fill
               sizes="100%"
               alt="About mentor team"
