c0c61650c3118a8124edbcc80c39f564b557227f
Brodich-vlad <114187879+Brodich-vlad@users.noreply.github.com>
2024-06-10 17:35:53 +0300

    Bugfix/join project section (#109)
    * fix JoinProjectSection.jsx
    
    * fix RegistrationFormModal

--- src/app/[locale]/ui-kit/page.jsx
+++ src/app/[locale]/ui-kit/page.jsx
@@ -1,5 +1,4 @@
 import FooterLinks from "@/src/components/shared/FooterLinks/FooterLinks";
-import PartnerCard from "@/src/components/shared/PartnerCard/PartnerCard";
 import SocialIcons from "@/src/components/shared/SocialIcons/SocialIcons";
 import HeroCard from "@/src/components/shared/HeroCard/HeroCard";
 import RegistrationFormModal from "@/src/components/RegistrationFormModal/RegistrationFormModal";
@@ -11,7 +10,6 @@ export default function UiKit() {
     <main>
       <FooterLinks />
       <ControlBtnRegistrationFormModal>Test RegistrationFormModal</ControlBtnRegistrationFormModal>
-      <PartnerCard />
       <HeroCard
         title="slide_1.title"
         desc="slide_1.text"
