3675a613aa67c06a5ba5ce1e8e45b521c2f01bdc
artyomakulov <artyomakulov@gmail.com>
2024-06-11 21:35:42 +0300

    Update AdvantagesSection.jsx

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -4,8 +4,10 @@ import Advantage from "../shared/Advantage/Advantage";
 import { items } from "./Items";
 import styles from "./AdvantagesSection.module.scss";
 import ControlBtnRegistrationFormModal from "../shared/controlButtons/ControlBtnRegistrationFormModal";
+import { useTranslations } from "next-intl";
 
 const AdvantagesSection = () => {
+  const t = useTranslations();
   return (
     <div className={styles.section}>
       <h3 className={styles.title}>Переваги стажування</h3>
