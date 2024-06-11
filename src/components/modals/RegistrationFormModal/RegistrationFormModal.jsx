86a920e30843f8b0d406bcf3d770b8e5182a99de
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-11 17:31:27 +0300

    fix content messages/ua/en.json

--- src/components/modals/RegistrationFormModal/RegistrationFormModal.jsx
+++ src/components/modals/RegistrationFormModal/RegistrationFormModal.jsx
@@ -1,16 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./RegistrationFormModal.module.scss";
+import clsx from "clsx";
 import { useForm } from "react-hook-form";
 import { useTranslations } from "next-intl";
 import { formScheme } from "./formScheme";
 import stateRegistrationFormModal from "@/src/state/stateRegistrationFormModal";
 import { useBodyLock } from "@/src/lib/hooks/useBodyLock";
-//import Modal from "../LayoutModal/LayoutModal";
+
 import CloseBtn from "../../shared/CloseBtn/CloseBtn";
 import MainButton from "../../shared/MainButton/MainButton";
 import InputField from "../../shared/InputField/InputField";
 import LayoutModal from "../LayoutModal/LayoutModal";
+import { Icon } from "../../shared/Icon/Icon";
 
 const RegistrationFormModal=({test=false})=>{
   const t = useTranslations("Modal_form");
@@ -26,6 +28,9 @@ const RegistrationFormModal=({test=false})=>{
   // Отримуємо стан.
   const isOpen = stateRegistrationFormModal(state => state.isOpen);
   const onClose = stateRegistrationFormModal(state => state.close);
+
+  const [ specialization, setSpecialization ] = useState('')
+  const [ convenientTime, setConvenientTime ] = useState('')
 
   useBodyLock(isOpen);
 
@@ -46,8 +51,8 @@ const RegistrationFormModal=({test=false})=>{
 
   const optionsTime = [
     { id: "t9001200", label: "9.00-12.00" },
-    { id: "t18002100", label: "18.00-21.00" },
     { id: "t12001500", label: "12.00-15.00" },
+    { id: "t18002100", label: "18.00-21.00" },
     { id: "anytime", label: t("anytime") },
   ];
 
@@ -146,7 +151,7 @@ const RegistrationFormModal=({test=false})=>{
         <div className={styles.modal} onClick={(e) => e.stopPropagation()
         }>
 
-        <CloseBtn className={styles.closeButton}
+          <CloseBtn className={styles.closeButton}
           onClick={onClose}/>
 
          {test ? 
@@ -154,14 +159,175 @@ const RegistrationFormModal=({test=false})=>{
              :
           <form onSubmit={handleSubmit(onSubmit)}>
             <h2>{t("title_mentor")}</h2>
+
             <ul className={styles.list}>
-              {inputFields.map((field) => (
-                <div key={field.id}>
-                  <InputField {...field} />
-                  {field.isError && <p>{t(`error_message.${field.id}`)}</p>}
+              <li>
+                <InputField
+                  id={"firstName"}
+                  placeholder={t("firstName")}
+                  registerOptions={register("firstName", { ...formScheme.firstName, onBlur:() => {
+                    trigger("firstName")
+                  }})}
+                  isError={errors.firstName}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("firstName")}
+                />
+
+                {errors.firstName && <span className={clsx(styles.error, styles._hide)}>{t("error_message.firstName")}</span>}
+              </li>
+
+              <li>
+                <InputField
+                  id={"lastName"}
+                  placeholder={t("lastName")}
+                  registerOptions={register("lastName", { ...formScheme.lastName, onBlur:() => {
+                    trigger("lastName")
+                  }})}
+                  isError={errors.firstName}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("lastName")}
+                />
+
+                {errors.lastName && <span className={clsx(styles.error, styles._hide)}>{t("error_message.lastName")}</span>}
+              </li>
+
+              <li>
+                <div className={styles.item}>
+                  <h4>{t("specialization")} <span>*</span></h4>
+                  <div className={styles.select}>
+                    {optionsSpec.map((option)=>{
+                      return(
+                        <label for={option.id} className={styles.btn_option} key={option.id}>
+                        <input 
+                        type="radio" 
+                        {...register("specialization", { ...formScheme.specialization })}
+                        id={option.id} name="specialization" 
+                        value={option.label} 
+                        onClick={()=>{setSpecialization(option.label)}}/>
+                          <span className={clsx(styles.check,specialization === option.label && styles._active)}>
+                            <Icon name={'check'}/>
+                          </span>
+                          {option.label}
+                        </label>
+                      )
+                    })}
+                  </div>
+                  
+                  {errors.specialization && <span className={clsx(styles.error, styles._hide)}>{t("error_message.specialization")}</span>}
                 </div>
-              ))}
+
+                {/* <InputField {...{version: "checkBox",
+                    id: "specialization",
+                    options: optionsSpec,
+                    control,
+                    label: t("specialization")}}
+                  // id={'specialization'}
+                  // placeholder={t("specialization")}
+                  // registerOptions={register("firstName", { ...formScheme.lastName, onBlur:() => {
+                  //   trigger("lastName")
+                  // }})}
+                  // isError={errors.firstName}
+                  // isValid={isValid}
+                  // version={"checkBox"}
+                  // label={t("specialization")}
+                />
+
+                {errors.lastName && <span className={clsx(styles.error, styles._hide)}>{t("error_message.lastName")}</span>} */}
+              </li>
+
+              <li>
+                <InputField
+                  id={"email"}
+                  placeholder={"email@gmail.com"}
+                  registerOptions={register("email", { ...formScheme.email,onBlur:() => {
+                    trigger("email")
+                  } })}
+                  isError={errors.email}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("email")}
+                />
+                {errors.email && <span className={clsx(styles.error, styles._hide)}>{t("error_message.email")}</span>}
+              </li>
+              <li>
+                <InputField
+                  id={"phone"}
+                  placeholder={"+380 xx xxx xx xx"}
+                  registerOptions={register("phone", { ...formScheme.phone,onBlur:() => {
+                    trigger("phone")
+                  } })}
+                  isError={errors.phone}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("phone")}
+                />
+                {errors.phone && <span className={clsx(styles.error, styles._hide)}>{t("error_message.phone")}</span>}
+              </li>
+              <li>
+                <InputField
+                  id={"discord"}
+                  placeholder={"+380 xx xxx xx xx"}
+                  registerOptions={register("discord", { ...formScheme.discord,onBlur:() => {
+                    trigger("discord")
+                  } })}
+                  isError={errors.discord}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("discord")}
+                />
+                {errors.discord && <span className={clsx(styles.error, styles._hide)}>{t("error_message.discord")}</span>}
+              </li>
+              <li>
+                <InputField
+                  id={"linkedin"}
+                  placeholder={t("linkedin_placeholder")}
+                  registerOptions={register("linkedin", { ...formScheme.linkedin, onBlur:() => {
+                    trigger("linkedin")
+                  } })}
+                  isError={errors.linkedin}
+                  isValid={isValid}
+                  version={"input"}
+                  label={t("linkedin")}
+                />
+                {errors.linkedin && <span className={clsx(styles.error, styles._hide)}>{t("error_message.linkedin")}</span>}
+              </li>
+
+              <li>
+                <div className={styles.item}>
+                  <h4>{t("convenient_time")} <span>*</span></h4>
+                  <div className={styles.select}>
+                    {optionsTime.map((option)=>{
+                      return(
+                        <label for={option.id} className={styles.btn_option} key={option.id}>
+                        <input 
+                        type="radio" 
+                        {...register("convenient_time", { ...formScheme.convenient_time })}
+                        id={option.id} name="convenient_time" 
+                        value={option.label} 
+                        onClick={()=>{setConvenientTime(option.label)}}/>
+                          <span className={clsx(styles.check, convenientTime === option.label && styles._active)}>
+                            <Icon name={'check'}/>
+                          </span>
+                          {option.label}
+                        </label>
+                      )
+                    })}
+                  </div>
+                  
+                  {errors.convenient_time && <span className={clsx(styles.error, styles._hide)}>{t("error_message.convenient_time")}</span>}
+                </div>
+
+              </li>
+                {/* {inputFields.map((field) => (
+                  <div key={field.id}>
+                    <InputField {...field} />
+                    {field.isError && <p>{t(`error_message.${field.id}`)}</p>}
+                  </div>
+                ))} */}
             </ul>
+
             <MainButton
               type="submit"
              // disabled={!isDirty || !isValid}
