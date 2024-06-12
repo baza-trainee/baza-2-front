89999db6657ac7cb52250f22d37d467262c25928
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2024-06-12 08:55:39 +0300

    Feature/Advantages (#112)
    * ceate Advantage component, add Advantage section
    
    create components
    add styles
    add images
    
    * add responsive
    
    * add Button
    
    add button
    
    * fix of laptope vers
    
    * Update AdvantagesSection.jsx
    
    * fix of text width

---
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Advantage from "../shared/Advantage/Advantage";
+import { items } from "./Items";
+import styles from "./AdvantagesSection.module.scss";
+import ControlBtnRegistrationFormModal from "../shared/controlButtons/ControlBtnRegistrationFormModal";
+import { useTranslations } from "next-intl";
+
+const AdvantagesSection = () => {
+  const t = useTranslations();
+  return (
+    <div className={styles.section}>
+      <h3 className={styles.title}>Переваги стажування</h3>
+      <ul className={styles.list}>
+        {items.map(({ img, text }) => (
+          <li key={img}>
+            <Advantage img={img} text={text} />
+          </li>
+        ))}
+      </ul>
+      <div className={styles.button}>
+        <ControlBtnRegistrationFormModal>
+          Приєднатися до спільноти
+        </ControlBtnRegistrationFormModal>
+      </div>
+    </div>
+  );
+};
+export default AdvantagesSection;
---
+++ src/components/AdvantagesSection/Items.js
@@ -0,0 +1,26 @@
+export const items = [
+  {
+    img: "/images/advantages/1.png",
+    text: "Застосовуємо і підтверджуємо теоретичні знання",
+  },
+  {
+    img: "/images/advantages/2.png",
+    text: "Набуваємо досвіду роботи в команді",
+  },
+  {
+    img: "/images/advantages/3.png",
+    text: "Позбуваємось невпевненості, знижуємо стрес",
+  },
+  {
+    img: "/images/advantages/4.png",
+    text: "Розуміємо, для чого виконується проєкт",
+  },
+  {
+    img: "/images/advantages/5.png",
+    text: "Формуємо портфоліо з реальними проектами",
+  },
+  {
+    img: "/images/advantages/6.png",
+    text: "Обираємо роботодавця свідомо",
+  },
+];
