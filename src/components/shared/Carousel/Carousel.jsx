8bdd62026cb2f3aaddd60e6054897170661fba87
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-12 19:17:42 +0300

    fix Carousel

--- src/components/shared/Carousel/Carousel.jsx
+++ src/components/shared/Carousel/Carousel.jsx
@@ -1,9 +1,95 @@
+// "use client";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation } from "swiper/modules";
+// import { useCallback } from "react";
+// import "swiper/css";
+// import { EffectCoverflow, Autoplay } from "swiper/modules";
+
+// const Carousel = ({
+//   modules,
+//   className,
+//   slideClassName,
+//   renderItem,
+//   items,
+//   prevEl,
+//   nextEl,
+//   paginationEl,
+//   delay = 3000,
+//   useCoverflow = false, 
+//   useAutoplay = false,
+//   ...options
+// }) => {
+//   const renderSlides = useCallback(
+//     (items) =>
+//       items?.map((item, i) => (
+//         <SwiperSlide className={slideClassName} key={i}>
+//           {renderItem(item, i)}
+//         </SwiperSlide>
+//       )),
+//     [slideClassName, renderItem]
+//   );
+
+//   const DEFAULT_MODULES = [Navigation, Autoplay];
+
+//   return (
+//     <Swiper
+//       // modules={[
+//       //   ...(modules ?? DEFAULT_MODULES), 
+//       //   ...(useCoverflow ? [EffectCoverflow] : []),
+//       //  ...(useAutoplay ? [Autoplay] : []),
+//       //  ]}
+//       modules={[...(modules ?? DEFAULT_MODULES)]}
+//       navigation={{
+//         prevEl: prevEl,
+//         nextEl: nextEl,
+//       }}
+//       pagination={{
+//         el: paginationEl ?? null,
+//         clickable: true,
+//       }}
+//       // autoplay={
+//       //   useAutoplay ? {
+//       //   delay: delay,
+//       //   disableOnInteraction: false,
+//       // } : false }
+
+//       autoplay={{
+//         delay: delay,
+//         disableOnInteraction: false,
+//       }}
+
+//       speed={700}
+//       className={className}
+//       {...options}
+
+//       effect={useCoverflow ? 'coverflow' : 'slide'} 
+//       grabCursor={useCoverflow}
+//       centeredSlides={useCoverflow}
+//       loop={true}
+//       coverflowEffect={
+//         useCoverflow
+//           ? {
+//               rotate: 0,
+//               stretch: 0,
+//               depth: 100,
+//               modifier: 2.5,
+//             }
+//           : {}
+//       }
+//     >
+//       {renderSlides(items)}
+//     </Swiper>
+//   );
+// };
+
+// export default Carousel;
+
+
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { useCallback } from "react";
 import "swiper/css";
-import { EffectCoverflow, Autoplay } from "swiper/modules";
 
 const Carousel = ({
   modules,
@@ -14,9 +100,8 @@ const Carousel = ({
   prevEl,
   nextEl,
   paginationEl,
-  delay = 3000,
-  useCoverflow = false, 
-  useAutoplay = false,
+  delay,
+  // slidesPerView: slidesPerViewProp = 1,
   ...options
 }) => {
   const renderSlides = useCallback(
@@ -28,16 +113,11 @@ const Carousel = ({
       )),
     [slideClassName, renderItem]
   );
-
-  const DEFAULT_MODULES = [Navigation, Autoplay];
+  const DEFAULT_MODULES = [Navigation];
 
   return (
     <Swiper
-      modules={[
-        ...(modules ?? DEFAULT_MODULES), 
-        ...(useCoverflow ? [EffectCoverflow] : []),
-       ...(useAutoplay ? [Autoplay] : []),
-       ]}
+      modules={[...(modules ?? DEFAULT_MODULES)]}
       navigation={{
         prevEl: prevEl,
         nextEl: nextEl,
@@ -46,29 +126,14 @@ const Carousel = ({
         el: paginationEl ?? null,
         clickable: true,
       }}
-      autoplay={
-        useAutoplay ? {
+      autoplay={{
         delay: delay,
         disableOnInteraction: false,
-      } : false }
+      }}
       speed={700}
+      // slidesPerView={slidesPerViewProp}
       className={className}
       {...options}
-
-      effect={useCoverflow ? 'coverflow' : 'slide'} 
-      grabCursor={useCoverflow}
-      centeredSlides={useCoverflow}
-      loop={true}
-      coverflowEffect={
-        useCoverflow
-          ? {
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }
-          : {}
-      }
     >
       {renderSlides(items)}
     </Swiper>
