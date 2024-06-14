51e489601be3f3162b2f1f883bbe8365cdc08600
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-06-14 12:11:39 +0300

    deleted effects civerFlowEffects, loop from Carousel component (#118)

--- src/components/shared/Carousel/Carousel.jsx
+++ src/components/shared/Carousel/Carousel.jsx
@@ -2,9 +2,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { useCallback } from "react";
-import "swiper/css";
-import { EffectCoverflow, Autoplay } from "swiper/modules";
-
+import "swiper/css"
 const Carousel = ({
   modules,
   className,
@@ -14,9 +12,8 @@ const Carousel = ({
   prevEl,
   nextEl,
   paginationEl,
-  delay = 3000,
-  useCoverflow = false, 
-  useAutoplay = false,
+  delay,
+  // slidesPerView: slidesPerViewProp = 1,
   ...options
 }) => {
   const renderSlides = useCallback(
@@ -28,17 +25,10 @@ const Carousel = ({
       )),
     [slideClassName, renderItem]
   );
-
-  const DEFAULT_MODULES = [Navigation, Autoplay];
-
+  const DEFAULT_MODULES = [Navigation]
   return (
     <Swiper
-      modules={[
-        ...(modules ?? DEFAULT_MODULES), 
-        ...(useCoverflow ? [EffectCoverflow] : []),
-       ...(useAutoplay ? [Autoplay] : []),
-       ]}
-      // modules={[...(modules ?? DEFAULT_MODULES)]}
+      modules={[...(modules ?? DEFAULT_MODULES)]}
       navigation={{
         prevEl: prevEl,
         nextEl: nextEl,
@@ -47,97 +37,17 @@ const Carousel = ({
         el: paginationEl ?? null,
         clickable: true,
       }}
-      // autoplay={
-      //   useAutoplay ? {
-      //   delay: delay,
-      //   disableOnInteraction: false,
-      // } : false }
-
       autoplay={{
         delay: delay,
         disableOnInteraction: false,
       }}
-
       speed={700}
+      // slidesPerView={slidesPerViewProp}
       className={className}
       {...options}
-
-      effect={useCoverflow ? 'coverflow' : 'slide'} 
-      grabCursor={useCoverflow}
-      centeredSlides={useCoverflow}
-      loop={true}
-      coverflowEffect={
-        useCoverflow
-          ? {
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }
-          : {}
-      }
     >
       {renderSlides(items)}
     </Swiper>
   );
-};
-
+}
 export default Carousel;
-
-
-// "use client";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation } from "swiper/modules";
-// import { useCallback } from "react";
-// import "swiper/css";
-
-// const Carousel = ({
-//   modules,
-//   className,
-//   slideClassName,
-//   renderItem,
-//   items,
-//   prevEl,
-//   nextEl,
-//   paginationEl,
-//   delay,
-//   // slidesPerView: slidesPerViewProp = 1,
-//   ...options
-// }) => {
-//   const renderSlides = useCallback(
-//     (items) =>
-//       items?.map((item, i) => (
-//         <SwiperSlide className={slideClassName} key={i}>
-//           {renderItem(item, i)}
-//         </SwiperSlide>
-//       )),
-//     [slideClassName, renderItem]
-//   );
-//   const DEFAULT_MODULES = [Navigation];
-
-//   return (
-//     <Swiper
-//       modules={[...(modules ?? DEFAULT_MODULES)]}
-//       navigation={{
-//         prevEl: prevEl,
-//         nextEl: nextEl,
-//       }}
-//       pagination={{
-//         el: paginationEl ?? null,
-//         clickable: true,
-//       }}
-//       autoplay={{
-//         delay: delay,
-//         disableOnInteraction: false,
-//       }}
-//       speed={700}
-//       // slidesPerView={slidesPerViewProp}
-//       className={className}
-//       {...options}
-//     >
-//       {renderSlides(items)}
-//     </Swiper>
-//   );
-// };
-
-// export default Carousel;
