a8e191008e330249437ead02239a4216cf7cf8f7
Богдан <sysoevbogdan82@gmail.com>
2024-06-14 16:17:35 +0300

    fix/fixed message fiels. Also fixed counter and FAQ sections

--- src/components/HistorySection/Timeline/Timeline.jsx
+++ src/components/HistorySection/Timeline/Timeline.jsx
@@ -2,57 +2,64 @@
 
 import { useEffect, useRef, useState } from "react";
 import { useTranslations } from "next-intl";
+import { motion, useScroll } from "framer-motion";
 import clsx from "clsx";
 import data from "./data";
 import styles from "./timeline.module.scss";
 
 const Timeline = () => {
   const t = useTranslations("Main.history_project_section.history");
-
-  const [fillHeight, setFillHeight] = useState(0);
-  const [activeIndex, setActiveIndex] = useState(-1);
   const sectionRef = useRef(null);
-  const itemsRef = useRef([]);
+  const { scrollYProgress } = useScroll({
+    container: sectionRef,
+  });
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (!sectionRef.current) return;
+  // const [fillHeight, setFillHeight] = useState(0);
+  // const [activeIndex, setActiveIndex] = useState(-1);
+  // const itemsRef = useRef([]);
 
-      const sectionTop =
-        sectionRef.current.getBoundingClientRect().top + window.scrollY;
-      const sectionHeight = sectionRef.current.offsetHeight;
-      const windowHeight = window.innerHeight;
-      const scrollCenter = window.scrollY + windowHeight / 2;
+  // useEffect(() => {
+  // const handleScroll = () => {
+  // if (!sectionRef.current) return;
 
-      const fill = Math.max(
-        Math.min(scrollCenter - sectionTop, sectionHeight),
-        0
-      );
-      setFillHeight(fill);
+  // const sectionTop =
+  //   sectionRef.current.getBoundingClientRect().top + window.scrollY;
+  // const sectionHeight = sectionRef.current.offsetHeight;
+  // const windowHeight = window.innerHeight;
+  // const scrollCenter = window.scrollY + windowHeight / 2;
 
-      let newIndex = -1;
-      itemsRef.current.forEach((item, index) => {
-        const itemTop =
-          item.getBoundingClientRect().top + window.scrollY - sectionTop;
-        if (fill >= itemTop) {
-          newIndex = index;
-        }
-      });
+  // const fill = Math.max(
+  //   Math.min(scrollCenter - sectionTop, sectionHeight),
+  //   0
+  // );
+  // setFillHeight(scrollYProgress * 100);
 
-      setActiveIndex(newIndex);
-    };
+  // let newIndex = -1;
+  // itemsRef.current.forEach((item, index) => {
+  //   const itemTop =
+  //     item.getBoundingClientRect().top + window.scrollY - sectionTop;
+  //   if (fill >= itemTop) {
+  //     newIndex = index;
+  //   }
+  // });
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  // setActiveIndex(newIndex);
+  // };
+
+  // window.addEventListener("scroll", handleScroll);
+  // return () => {
+  // window.removeEventListener("scroll", handleScroll);
+  // };
+  // }, []);
+
   return (
     <div ref={sectionRef} className={styles.wrapper}>
       <div className={styles.timeline}>
         <div className={styles.timeline__initial}>
-          <div
-            style={{ height: `${fillHeight}px` }}
+          <motion.div
+            style={{
+              scaleY: scrollYProgress,
+            }}
             className={clsx(styles.timeline__draw)}
           />
         </div>
@@ -60,10 +67,10 @@ const Timeline = () => {
         <ul className={styles.timeline__list}>
           {data.map((item, index) => (
             <li
-              ref={(el) => (itemsRef.current[index] = el)}
+              // ref={(el) => (itemsRef.current[index] = el)}
               className={clsx(
-                styles.timeline__list__element,
-                activeIndex >= index && styles.active
+                styles.timeline__list__element
+                // activeIndex >= index && styles.active
               )}
               style={{ gridRow: ++index }}
               id={item.id}
