a6566e8533b7a513d52acc9149817ab325e1eedc
Anastasiia <128808242+Anastasiia-Ihn@users.noreply.github.com>
2024-06-16 07:59:40 +0200

    change btn to top, image (#129)

--- src/app/[locale]/projects/page.jsx
+++ src/app/[locale]/projects/page.jsx
@@ -1,9 +1,12 @@
+import ScrollToTopBtn from "@/src/components/shared/ScrollToTopBtn/ScrollToTopBtn";
+
 export default function Projects() {
   // !! Замінити  <div> на готові компоненти !!
  
   return (
     <main className="container" style={{marginTop:'120px'}}>
-      <div  style={{marginBottom:'120px'}}><h2>Сторінка: Проекти</h2></div>
+      <div style={{ marginBottom: '120px' }}><h2>Сторінка: Проекти</h2></div>
+      <ScrollToTopBtn/>
     </main>
   );
 };
