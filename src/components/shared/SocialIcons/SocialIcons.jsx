5a21662e6b3480862bac8d5836ec1aa98b634649
artyomakulov <artyomakulov@gmail.com>
2024-06-16 12:13:13 +0300

    fix of styles

--- src/components/shared/SocialIcons/SocialIcons.jsx
+++ src/components/shared/SocialIcons/SocialIcons.jsx
@@ -6,14 +6,15 @@ const SocialIcons = () => {
     <div className={styles.list}>
       <a
         href="https://www.linkedin.com/company/baza-trainee-ukraine/"
+        target="_blank"
         className={styles.link}
       >
         <Icon name="linkedin" width={48} height={48} className={styles.icon} />
       </a>
-      <a href="https://www.facebook.com/BazaIT/">
+      <a href="https://www.facebook.com/BazaIT/" target="_blank">
         <Icon name="facebook" width={48} height={48} className={styles.icon} />
       </a>
-      <a href="https://t.me/+CBXkAJlsCy83ZDYy">
+      <a href="https://t.me/+CBXkAJlsCy83ZDYy" target="_blank">
         <Icon name="telegram" width={48} height={48} className={styles.icon} />
       </a>
     </div>
