5cf79899600c983526af47dd5f5f148eeec3ab04
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2024-06-19 08:40:35 +0300

    add translation (#138)

--- src/components/AdvantagesSection/Items.js
+++ src/components/AdvantagesSection/Items.js
@@ -1,26 +1,26 @@
 export const items = [
   {
     img: "/images/advantages/1.png",
-    text: "Застосовуємо і підтверджуємо теоретичні знання",
+    text: "item_1",
   },
   {
     img: "/images/advantages/2.png",
-    text: "Набуваємо досвіду роботи в команді",
+    text: "item_2",
   },
   {
     img: "/images/advantages/3.png",
-    text: "Позбуваємось невпевненості, знижуємо стрес",
+    text: "item_3",
   },
   {
     img: "/images/advantages/4.png",
-    text: "Розуміємо, для чого виконується проєкт",
+    text: "item_4",
   },
   {
     img: "/images/advantages/5.png",
-    text: "Формуємо портфоліо з реальними проектами",
+    text: "item_5",
   },
   {
     img: "/images/advantages/6.png",
-    text: "Обираємо роботодавця свідомо",
+    text: "item_6",
   },
 ];
