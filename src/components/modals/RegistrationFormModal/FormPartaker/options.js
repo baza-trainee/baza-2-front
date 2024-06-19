1564ee8e84f26b84e81bc712c9530756a3c8da21
Brodich-vlad <114187879+Brodich-vlad@users.noreply.github.com>
2024-06-19 17:28:21 +0300

    Feature/form partaker (#141)
    * fix style FormMentor
    
    * fix UseAlert
    
    * add content messages/en.json
    
    * add formatPhoneNumber
    
    * fix FormMentor
    
    * add FormPartaker
    
    * bag fix FormMentor
    
    * bag fix LayoutModal
    
    * fix messages/en.json
    
    * add content messages/ua.json
    
    * fix content messages/ua.json
    
    * fix PaymentModal
    
    * add content FormPartaker

--- src/components/modals/RegistrationFormModal/FormPartaker/options.js
+++ src/components/modals/RegistrationFormModal/FormPartaker/options.js
@@ -2,17 +2,15 @@ export const optionsSpec = [
   { id: "uiuxDesigner", label: "UI/UX Designer" },
   { id: "backend", label: "Backend" },
   { id: "frontend", label: "Frontend" },
- 
   { id: "fullstackEngineer", label: "Fullstack Engineer" },
   { id: "qaManualEngineer", label: "QA Manual Engineer" },
   { id: "projectManager", label: "Project Manager" },
 ];
 
-export const optionsTime = [
-  { id: "time1", label: "9.00-12.00" },
-  { id: "time2", label: "12.00-15.00" },
-  { id: "time3", label: "18.00-21.00" },
-  { id: "time4", label: "anytime" },
+export const optionsQuestionnaire = [
+  { id: "questionnaire_options.option_1"},
+  { id: "questionnaire_options.option_2"},
+  { id: "questionnaire_options.option_3"},
+  { id: "questionnaire_options.option_4"},
+  { id: "questionnaire_options.option_5"},
 ];
-
-//export const optionsPersonalData = [{ id: "personaldata", label: t("permit") }];
