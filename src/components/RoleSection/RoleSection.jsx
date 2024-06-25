fe71508b3f3e7c367c684f37155b29bbdf00bdd7
Brodich Vlad <brodich_vlad@ukr.net>
2024-06-25 09:28:33 +0300

    test RoleSection

--- src/components/RoleSection/RoleSection.jsx
+++ src/components/RoleSection/RoleSection.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { items } from "./items";
 import RoleCard from "../shared/RoleCard/RoleCard";
@@ -5,6 +6,8 @@ import ControlBtnRegistrationFormModal from "../shared/controlButtons/ControlBtn
 import { useTranslations } from "next-intl";
 import { createKey } from "@/src/lib/utils/createKey";
 import styles from "./RoleSection.module.scss";
+import Carousel from "../shared/Carousel/Carousel";
+//import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 const RoleSection = () => {
   const t = useTranslations("Internship.invite_participate");
@@ -13,6 +16,32 @@ const RoleSection = () => {
     <section className={styles.roleSection}>
       <div className={styles.container}>
         <h2 className={styles.title}>{t("title")}</h2>
+
+        {/* слайдер на мобільному */}
+        <div className={styles.mobile_content}>
+        <Carousel
+        //modules={[Navigation, Pagination, Autoplay]}
+        //paginationEl={".custom-pagination-hero"}
+        items={items}
+        spaceBetween={10}
+        // delay={10000}
+        // breakpoints={{
+        //   992: {
+        //     sped:100,
+        //   },
+        // }}
+        renderItem={(item) => (
+          <RoleCard
+          key={createKey()}
+          title={item.title}
+          imgSrc={item.imgSrc}
+          desc1={t(item.desc1)}
+          desc2={t(item.desc2)}
+        />
+        )}
+      />
+        {/* грід все інше */}
+        </div>
         <div className={styles.content}>
           {items.map((item) => (
             <RoleCard
---
+++ Лого партнер/partnerCardItems.js
@@ -0,0 +1,89 @@
+export const partnerCardItems = [
+  {
+    title: "tucha",
+    img: "/images/partner_section/tucha.png",
+    url: "https://tucha.ua/uk",
+  },
+  {
+    title: "slide_2.title",
+    img: "/images/partner_section/patronbadge.png",
+    url: "https://patronbadge.com/",
+  },
+  {
+    title: "slide_3.title",
+    img: "/images/partner_section/educate.svg",
+    url: "/test",
+  },
+  {
+    title: "skill",
+    img: "/images/partner_section/skill.svg",
+    url: "https://baza-skill.com.ua/",
+  },
+  // {
+  //   title: "Baza Poligon",
+  //   img: "/images/partner_section/poligon",
+  //   url: "/test",
+  // },
+  {
+    title: "deltahost",
+    img: "/images/partner_section/deltahost.svg",
+    url: "https://deltahost.com/",
+  },
+
+  {
+    title: "Академія наук Ураїни",
+    img: "/images/partner_section/academySciencesUkraine.png",
+    url: "https://www.nas.gov.ua/",
+  },
+  {
+    title: "status",
+    img: "/images/partner_section/status.svg",
+    url: "https://statusac.com.ua/",
+  },
+  {
+    title: "tails",
+    img: "/images/partner_section/tails.webp",
+    url: "hhttps://martys-dogs.vercel.app/"
+  },
+  {
+    title: "zerno",
+    img: "/images/partner_section/zerno.png",
+    url: "https://www.zerno.fashion/",
+  },
+  {
+    title: "juniverse",
+    img: "/images/partner_section/juniverse.svg",
+    url: "https://juniverse.com.ua/",
+  },
+  {
+    title: "charity_tuner",
+    img: "/images/partner_section/charity_tuner.png",
+    url: "http://charitytuner.org/"
+  },
+  {
+    title: "scsa",
+    img: "/images/partner_section/scsa.png",
+    url: "https://scsa.org.ua/",
+  },
+  {
+    title: "HostIQ",
+    img: "/images/partner_section/hostIq.png",
+    url: "https://hostiq.ua/",
+  },
+  {
+    title: "eng-for",
+    img: "/images/partner_section/eng_for.svg",
+    url: "https://hostiq.ua/",
+  },
+  {
+    title: "martha-house",
+    img: "/images/partner_section/martha_house.png",
+    url: "https://martha-house.vercel.app/",
+  },
+  {
+    title: "1001 songs",
+    img: "/images/partner_section/songs_1001.png",
+    url: "https://baza-trainee.github.io/1001-songs-frontend/",
+  },
+];
+// https://martha-house.vercel.app/
