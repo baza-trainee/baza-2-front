c24355064be6a76a3732b38f2e9cdc3bbc8881a7
Brodich Vlad <brodich_vlad@ukr.net>
2024-07-01 13:40:36 +0300

    add badges

--- src/state/stateUseAlert.js
+++ src/state/stateUseAlert.js
@@ -2,10 +2,18 @@ import { create } from 'zustand'
 // type 'error'|'success'|'info';
 const stateUseAlert = create((set) => ({
   isOpen: false,
-  type:'error',
-  autoClose:true,
-  open: (typeName ='error',autoClose = true) => set({ isOpen: true, type:typeName, autoClose:autoClose}),
-  close: () => set({ isOpen: false, type:'error', autoClose:true }),
+  type: 'error',
+  autoClose: true,
+  open: (typeName ='error', autoClose = true) => set({ 
+    isOpen: true, 
+    type:typeName, 
+    autoClose:autoClose
+  }),
+  close: () => set({ 
+    isOpen: false, 
+    type:'error', 
+    autoClose:true 
+  }),
 }))
 
 export default stateUseAlert
