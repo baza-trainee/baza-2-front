f569edd5a95c1a23e633e20b53d32803841a9b25
Brodich Vlad <brodich_vlad@ukr.net>
2024-07-06 17:50:10 +0300

    fix createImageUrl

--- src/lib/hooks/createImageUrl.js
+++ src/lib/hooks/createImageUrl.js
@@ -1,5 +1,5 @@
 import { BASE_URL, BASE_URL2 } from "@/src/constants/constants"
 
-export const createImageUrl = (id) =>{return `${BASE_URL2}/v1/files/${id}`}
+export const createImageUrl = (id) =>{return `${BASE_URL2}/files/${id}`}
 
 export const createImageUrlBaza1 = (id) =>{return `${BASE_URL}/files/${id}`}
