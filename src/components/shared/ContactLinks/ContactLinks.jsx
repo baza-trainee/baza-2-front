de5a7764f34ac2135daeff59b78a100a5650135d
artyomakulov <artyomakulov@gmail.com>
2024-07-06 15:15:26 +0300

    add contacts
    add contacts and social media from back end

--- src/components/shared/ContactLinks/ContactLinks.jsx
+++ src/components/shared/ContactLinks/ContactLinks.jsx
@@ -2,22 +2,54 @@
 import Image from "next/image";
 import styles from "./ContactLinks.module.scss";
 import { Icon } from "../Icon/Icon";
+import { useEffect, useState } from "react";
+import { getContacts } from "@/src/api/contacts";
 
 const ContactLinks = () => {
+  const [contactsData, setContactsData] = useState(null);
+
+  useEffect(() => {
+    getContacts()
+      .then((data) => {
+        console.log("Contacts data:", data);
+        setContactsData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching contacts:", error.message);
+      });
+  }, []);
+
+  const formatPhoneNumber = (phoneNumber) => {
+    const cleaned = ("" + phoneNumber).replace(/\D/g, "");
+    const match = cleaned.match(/^(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})$/);
+    if (match) {
+      return `+${match[1]} ${match[2]} ${match[3]} ${match[4]} ${match[5]}`;
+    }
+    return phoneNumber;
+  };
+
   return (
     <div className={styles.contacts}>
-      <div className={styles.contact}>
-        <Icon name="phone" width={25} height={29} />
-        <p className={styles.text}>+380 63 628 66 30</p>
-      </div>
-      <div className={styles.contact}>
-        <Icon name="phone" width={25} height={29} />
-        <p className={styles.text}>+380 67 568 17 88</p>
-      </div>
-      <div className={styles.contact}>
-        <Icon name="mail" width={25} height={29} className={styles.mail} />
-        <p className={styles.text}>info@‌baza-trainee.tech</p>
-      </div>
+      {contactsData && (
+        <>
+          <div className={styles.contact}>
+            <Icon name="phone" width={25} height={29} />
+            <p className={styles.text}>
+              {formatPhoneNumber(contactsData.contactsDataList.phone1)}
+            </p>
+          </div>
+          <div className={styles.contact}>
+            <Icon name="phone" width={25} height={29} />
+            <p className={styles.text}>
+              {formatPhoneNumber(contactsData.contactsDataList.phone2)}
+            </p>
+          </div>
+          <div className={styles.contact}>
+            <Icon name="mail" width={25} height={29} className={styles.mail} />
+            <p className={styles.text}>{contactsData.contactsDataList.email}</p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/shared/SocialIcons/SocialIcons.jsx
+++ src/components/shared/SocialIcons/SocialIcons.jsx
@@ -1,23 +1,56 @@
 import clsx from "clsx";
 import { Icon } from "../Icon/Icon";
 import styles from "./SocialIcons.module.scss";
+import { useEffect, useState } from "react";
+import { getContacts } from "@/src/api/contacts";
 
 const SocialIcons = ({ classNameCustom }) => {
+  const [socialMediaData, setSocialMediaData] = useState(null);
+  useEffect(() => {
+    getContacts()
+      .then((data) => {
+        console.log("Contacts data:", data);
+        setSocialMediaData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching contacts:", error.message);
+      });
+  }, []);
+
   return (
     <div className={clsx(styles.list, classNameCustom)}>
-      <a
-        href="https://www.linkedin.com/company/baza-trainee-ukraine/"
-        target="_blank"
-        className={styles.link}
-      >
-        <Icon name="linkedin" width={48} height={48} className={styles.icon} />
-      </a>
-      <a href="https://www.facebook.com/BazaIT/" target="_blank">
-        <Icon name="facebook" width={48} height={48} className={styles.icon} />
-      </a>
-      <a href="https://t.me/+CBXkAJlsCy83ZDYy" target="_blank">
-        <Icon name="telegram" width={48} height={48} className={styles.icon} />
-      </a>
+      {socialMediaData && (
+        <>
+          <a
+            href={socialMediaData.socialsMediaList.linkedin}
+            target="_blank"
+            className={styles.link}
+          >
+            <Icon
+              name="linkedin"
+              width={48}
+              height={48}
+              className={styles.icon}
+            />
+          </a>
+          <a href={socialMediaData.socialsMediaList.facebook} target="_blank">
+            <Icon
+              name="facebook"
+              width={48}
+              height={48}
+              className={styles.icon}
+            />
+          </a>
+          <a href={socialMediaData.socialsMediaList.telegram} target="_blank">
+            <Icon
+              name="telegram"
+              width={48}
+              height={48}
+              className={styles.icon}
+            />
+          </a>
+        </>
+      )}
     </div>
   );
 };
