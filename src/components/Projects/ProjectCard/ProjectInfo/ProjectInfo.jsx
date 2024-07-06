77b4b45169e24b7addd5c95bd99ab11365833153
Богдан <sysoevbogdan82@gmail.com>
2024-07-06 20:32:04 +0300

    feat: some fixes for project card

--- src/components/Projects/ProjectCard/ProjectInfo/ProjectInfo.jsx
+++ src/components/Projects/ProjectCard/ProjectInfo/ProjectInfo.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import styles from "./ProjectInfo.module.scss";
 import { createKey } from "@/src/lib/utils/createKey";
 import clsx from "clsx";
+import { Icon } from "@/src/components/shared/Icon/Icon";
 
 const ProjectInfo = ({ complexity }) => {
   return (
     <div className={styles.info}>
       <div className={styles.infoRow}>
         <div className={styles.name}>
-          <span></span>Старт проекту
+          <Icon name="calendar" />
+          <span>Старт проекту</span>
         </div>
         <p>15 cічня 2024</p>
       </div>
       <div className={styles.infoRow}>
         <div className={styles.name}>
-          <span></span>Тривалість
+          <Icon name="clock" />
+          <span>Тривалість</span>
         </div>
         <p>6 тижнів</p>
       </div>
       <div className={styles.infoRow}>
         <div className={styles.name}>
-          <span></span>Складність
+          <Icon name="complexity" />
+          <span>Складність</span>
         </div>
         <div className={styles.complexity}>
           {Array.from({ length: 5 }, (_, index) => (
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,11 +3,14 @@ import ProjectCard from "./ProjectCard/ProjectCard";
 import { results } from "./items";
 import { createKey } from "@/src/lib/utils/createKey";
 import styles from "./Projects.module.scss";
+import InputField from "../shared/InputField/InputField";
 
 const Projects = () => {
   return (
-    <section className="section">
+    <section className={styles.section}>
       <div className={styles.projectsContainer}>
+        <h1 className={styles.title}>Projects</h1>
+        <InputField version={"input"} />
         <div className={styles.content}>
           {results.map((data) => (
             <ProjectCard key={createKey()} data={data} />
