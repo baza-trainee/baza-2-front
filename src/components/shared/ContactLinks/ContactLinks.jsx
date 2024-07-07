4dcca1f12fceda158d082385e52b72e504f1b81c
artyomakulov <artyomakulov@gmail.com>
2024-07-07 16:56:37 +0300

    add "use client"

--- src/components/shared/ContactLinks/ContactLinks.jsx
+++ src/components/shared/ContactLinks/ContactLinks.jsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import styles from "./ContactLinks.module.scss";
 import { Icon } from "../Icon/Icon";
 import { useEffect, useState } from "react";
--- src/components/shared/SocialIcons/SocialIcons.jsx
+++ src/components/shared/SocialIcons/SocialIcons.jsx
@@ -1,3 +1,4 @@
+"use client";
 import clsx from "clsx";
 import { Icon } from "../Icon/Icon";
 import styles from "./SocialIcons.module.scss";
