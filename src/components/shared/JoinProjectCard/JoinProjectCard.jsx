7e53756b62841e191679226413a1b52ebb32dc44
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-07-08 14:15:27 +0300

    fixed Cards layout and Styles in different Breakpoints

--- src/components/shared/JoinProjectCard/JoinProjectCard.jsx
+++ src/components/shared/JoinProjectCard/JoinProjectCard.jsx
@@ -16,27 +16,26 @@ export default function JoinProjectCard({ item = {}, i, progress, range, targetS
     offset: ['start end', 'start start']
   });
 
-  const isMobile = useMediaQuery({ maxWidth: 1365 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
-  const svgScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  // const svgScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
   const scale = useTransform(progress, range, [1, targetScale]);
 
   return (
     <div ref={container} className={styles.cardContainer}>
       <motion.div style={{ scale: isClient ? (isMobile ? 1 : scale) : 1, top: `calc(-10% + ${i * 50}px)` }} className={styles.card}>
-        <motion.div style={{ scale: isClient ? (isMobile ? 1 : svgScale) : 1 }} className={styles.svgWrapper}>
+        <div className={styles.svgWrapper}>
           <img
             className={styles.iconNumber}
-            height={250}
-            width={100}
+            fill sizes = "100%"
             src={icon}>
           </img>
-        </motion.div>
+        </div>
         <div className={styles.textCard}>
           <div className={styles.titleWrapper}>
             {t(title)}
