fed8b08b093e466813bfc9461b87737caeb2460b
Богдан <sysoevbogdan82@gmail.com>
2024-07-08 22:08:37 +0300

    fix: minor fixes for welcome section

--- src/components/RoleSection/items.js
+++ src/components/RoleSection/items.js
@@ -3,37 +3,37 @@ export const items = [
 		title: "UI/UX designer",
 		imgSrc: "/images/role_section/design.jpg",
 		desc1: 'card_1.text1',
-		desc2: 'card_1.text1',
+		desc2: 'card_1.text2',
 	},
 	{
 		title: "Frontend",
 		imgSrc: "/images/role_section/front.jpg",
 		desc1: 'card_2.text1',
-		desc2: 'card_2.text1',
+		desc2: 'card_2.text2',
 	},
 	{
 		title: "Backend",
 		imgSrc: "/images/role_section/back.jpg",
 		desc1: 'card_3.text1',
-		desc2: 'card_3.text1',
+		desc2: 'card_3.text2',
 	},
 	{
 		title: "Fullstack engineer",
 		imgSrc: "/images/role_section/fullstack.jpg",
 		desc1: 'card_4.text1',
-		desc2: 'card_4.text1',
+		desc2: 'card_4.text2',
 	},
 
 	{
 		title: "QA Manual engineer",
 		imgSrc: "/images/role_section/qa.jpg",
 		desc1: 'card_5.text1',
-		desc2: 'card_5.text1',
+		desc2: 'card_5.text2',
 	},
 	{
 		title: "Project Manager",
 		imgSrc: "/images/role_section/pm.jpg",
 		desc1: 'card_6.text1',
-		desc2: 'card_6.text1',
+		desc2: 'card_6.text2',
 	},
 ];
