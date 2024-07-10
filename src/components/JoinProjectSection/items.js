559c3c1acc31eb41c47dd18382c4c06d8a265c03
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-07-10 15:19:14 +0300

    fixed alt, added iteration for text, fixed semantic issuies

--- src/components/JoinProjectSection/items.js
+++ src/components/JoinProjectSection/items.js
@@ -4,28 +4,28 @@ export const items = [
     texts: ["card_1.text_1", "card_1.text_2", "card_1.text_3"],
     icon: "/images/icons/1.svg",
     image: "/images/icons/anketa.svg",
-    name: "anketa",
+    name: "step1",
   },
   {
     title: "card_2.title",
     texts: ["card_2.text_1", "card_2.text_2", "card_2.text_3"],
     icon: "/images/icons/2.svg",
     image: "/images/icons/invitation.svg",
-    name: "invitation",
+    name: "step2",
   },
   {
     title: "card_3.title",
     texts: ["card_3.text_1", "card_3.text_2", "card_3.text_3"],
     icon: "/images/icons/3.svg",
     image: "/images/icons/onboarding.svg",
-    name: "onboarding",
+    name: "step3",
   },
   {
     title: "card_4.title",
     texts: ["card_4.text_1", "card_4.text_2", "card_4.text_3"],
     icon: "/images/icons/4.svg",
     image: "/images/icons/communication.svg",
-    name: "communications",
+    name: "step4",
   },
 ];
 
