37f6b57017902fc2dc1e393d7001344ee541a63e
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2024-07-10 15:01:10 +0300

    fixed alt, added iteration for text, fixed semantic issuies

--- src/components/JoinProjectSection/items.js
+++ src/components/JoinProjectSection/items.js
@@ -1,34 +1,31 @@
 export const items = [
   {
     title: "card_1.title",
-    text_1: "card_1.text_1",
-    text_2: "card_1.text_2",
-    text_3: "card_1.text_3",
+    texts: ["card_1.text_1", "card_1.text_2", "card_1.text_3"],
     icon: "/images/icons/1.svg",
     image: "/images/icons/anketa.svg",
+    name: "anketa",
   },
   {
     title: "card_2.title",
-    text_1: "card_2.text_1",
-    text_2: "card_2.text_2",
-    text_3: "card_2.text_3",
+    texts: ["card_2.text_1", "card_2.text_2", "card_2.text_3"],
     icon: "/images/icons/2.svg",
     image: "/images/icons/invitation.svg",
+    name: "invitation",
   },
   {
     title: "card_3.title",
-    text_1: "card_3.text_1",
-    text_2: "card_3.text_2",
-    text_3: "card_3.text_3",
+    texts: ["card_3.text_1", "card_3.text_2", "card_3.text_3"],
     icon: "/images/icons/3.svg",
     image: "/images/icons/onboarding.svg",
+    name: "onboarding",
   },
   {
     title: "card_4.title",
-    text_1: "card_4.text_1",
-    text_2: "card_4.text_2",
-    text_3: "card_4.text_3",
+    texts: ["card_4.text_1", "card_4.text_2", "card_4.text_3"],
     icon: "/images/icons/4.svg",
     image: "/images/icons/communication.svg",
+    name: "communications",
   },
 ];
+
