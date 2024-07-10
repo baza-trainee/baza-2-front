e65991f052860aa2c2c0e02b1004fd8741189ba0
Brodich-vlad <114187879+Brodich-vlad@users.noreply.github.com>
2024-07-10 17:25:49 +0300

    Feature/tooltip text (#192)
    * add downloadPdf
    
    * add TooltipText
    
    * remove  package-lock.json
    
    * add package-lock.json
    
    * add example_discord

--- src/components/modals/RegistrationFormModal/TooltipText/TooltipText.jsx
+++ src/components/modals/RegistrationFormModal/TooltipText/TooltipText.jsx
@@ -1,22 +1,16 @@
 import styles from './TooltipText.module.scss'
 import clsx from 'clsx'
-import { Icon } from '@/src/components/shared/Icon/Icon'
-//На скріні - приклад!!!
+import Image from 'next/image'
+import { useTranslations } from 'use-intl';
+
 export default function TooltipText({className}) {
+  const t = useTranslations("Modal_form");
   return(
     <div className={clsx(styles.tooltiptext, className)}>
-      <p>Ім'я користувача в discord. 
-        Увага! Скопіюйте свій нік і надішліть в правильному вигляді! 
-      </p>
-
-      <div className={styles.example}>
-        <h3 className={styles.title}>Приклад:</h3>
-        <p className={styles.title}>ВІДОБРАЖУВАНЕ ІМ'Я</p>
-        <p className={clsx(styles.text, styles.error)}>Baza_Trainee_Ukraine_admin</p>
-      </div>
-      <div className={styles.example}>
-        <p className={styles.title}>ІМ'Я КОРИСТУВАЧА</p>
-        <p className={styles.text}>baza_trainee_ukraine_admin <span className={styles.icon}><Icon name={'check'} width={20} height={20}/></span></p>
+      <h4>{t("discord_tooltip_text")}</h4>
+      <p>{t("discord_tooltip_example")}</p>
+      <div className={styles.wrapp_img}>
+        <Image src={'/images/forms/example_discord.jpg'} fill sizes='100%' alt='example discord'/>
       </div>
     </div>
   )
