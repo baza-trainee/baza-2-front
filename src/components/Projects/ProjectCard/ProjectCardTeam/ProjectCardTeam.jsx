83d3a2be25fc354c1397f20a9cf15217f8f2439c
Sysoev Bogdan <112561940+Lundoger@users.noreply.github.com>
2024-07-12 08:25:41 +0300

    Feature/projects (#195)
    * feat: added state
    
    * feat: added new components for project card
    
    * feat: some fixes for project card
    
    * feat: added polish translition and finished project team

--- src/components/Projects/ProjectCard/ProjectCardTeam/ProjectCardTeam.jsx
+++ src/components/Projects/ProjectCard/ProjectCardTeam/ProjectCardTeam.jsx
@@ -3,6 +3,20 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import React from "react";
 import styles from "./ProjectCardTeam.module.scss";
+import { createKey } from "@/src/lib/utils/createKey";
+
+const orderList = [
+  "Product Owner",
+  "Project Manager",
+  "Project Manager Mentor",
+  "Business Analyst",
+  "Business Analyst Mentor",
+  "Design",
+  "Front-end",
+  "Back-end",
+  "Quality Assurance",
+  "Full Stack",
+];
 
 const ProjectCardTeam = ({ project, handleClose, isShowed }) => {
   const { locale } = useParams();
@@ -26,28 +40,34 @@ const ProjectCardTeam = ({ project, handleClose, isShowed }) => {
 
   return (
     <div className={clsx(styles.projectTeam, isShowed && styles.showed)}>
-      <h4 className={styles.title}>{projectTeamTitle}</h4>
-      <button className={styles.close} onClick={handleClose}>
-        <span />
-        <span />
-      </button>
-      <div className={styles.content}>
-        {sortedRoles.map((role) => (
-          <div key={role}>
-            <h4 className={styles.roleTitle}>{role}</h4>
+      <div className={styles.membersContainer}>
+        <div className={styles.titleRow}>
+          <h4 className={styles.title}>{projectTeamTitle}</h4>
+          <button className={styles.close} onClick={handleClose}>
+            <div className={styles.closeCnt}>
+              <span />
+              <span />
+            </div>
+          </button>
+        </div>
+        <div className={styles.content}>
+          {sortedRoles.map((role) => (
+            <div className={styles.members} key={createKey()}>
+              <h4 className={styles.titleRole}>{role}</h4>
 
-            {rolesAndMembers[role].map((teamMember) => (
-              <Link
-                key={teamMember._id}
-                className={styles.member}
-                href={teamMember?.profileUrl}
-                target="_blank"
-              >
-                {teamMember.name[locale]}
-              </Link>
-            ))}
-          </div>
-        ))}
+              {rolesAndMembers[role].map((teamMember) => (
+                <Link
+                  key={createKey()}
+                  className={styles.member}
+                  href={teamMember?.profileUrl}
+                  target="_blank"
+                >
+                  {teamMember.name[locale]}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
