cf2ea803ba02a231a476dbfdd7acf2f8ba544152
Brodich Vlad <brodich_vlad@ukr.net>
2024-07-24 16:14:28 +0300

    add test admin Counter

--- src/components/admin-page/Counter/Counter.jsx
+++ src/components/admin-page/Counter/Counter.jsx
@@ -2,70 +2,37 @@
 
 import styles from './Counter.module.scss'
 import HeaderAdmin from '../HeaderAdmin/HeaderAdmin'
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getEmployed, updateEmployed } from '@/src/api/achievements'
-import { useEffect } from 'react';
-import { getInfoUser } from '@/src/api/auth';
+import { useState } from 'react';
+import InputField from '../../shared/InputField/InputField';
+
 
 export default function Counter() {
+  const queryClient = useQueryClient();
+
   const { mutate, isPending, isError, data, error } = useMutation({
     mutationFn:(data) => {
       return updateEmployed(data)
-    }})
 
-    // useEffect(() => {
-    //   const getUser = async () => {
-    //     const response = await fetch(
-    //       `https://baza2.crabdance.com/api/v1/auth/user`,
-    //       { cache: 'no-store' }
-    //     );
-    //     if (response.ok) {
-    //       //router.push('/admin');
-    //       //setIsShow(true);
-    //     } else {
-    //       //router.push('/');
-    //     }
-    //     console.log(response)
-    //   };
-     
-    //   getUser();
-    // }, []);
-  
-  //   useEffect(() => {
-  //     const getUser = async () => {
-  //       const response = await fetch('https://baza2.crabdance.com/api/v1/auth/login', {
-  //   method: "POST",
-  //   credentials: "same-origin", 
-  //   body: JSON.stringify({"email": "user@example.com",
-  // "password": "password123"}),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // }).then((response) => {
-  //   return response.json();
-  // }).then((data) => {
-  //   console.log(data);
-  // })
-  //     };
-     
-  //     getUser();
-  //   }, []);
-  const employed = useQuery({ 
-    queryKey: ['InfoUser'], 
-    queryFn: getInfoUser
-  });
+    },onSuccess: () => {
+      queryClient.invalidateQueries('employed');
+    },})
 
- // console.log(employed.data)
-
-// if(!employed.data){return null}
-  //console.log(employed.data)
-
+    const [valueEmployed,setValueEmployed] = useState(1)
+    const employed = useQuery({ queryKey: ['employed'], queryFn: getEmployed });
+   
   return (
     <section className={styles.section}>
       <HeaderAdmin title={'Каунтер'}/>
       <div className={styles.srroll_wrapper}>
+       {employed.data && <h1>employed: {employed.data.employed}</h1>}
 
-      {<h1>employed: {employed.data.employed}</h1>} 
+       <InputField version={'input'} type='number'className={styles.input}  value={valueEmployed} onChange={(e)=>{setValueEmployed(e.target.value)}}/>
+        <p className={styles.btn} onClick={()=>{mutate({
+  "employed": Number(valueEmployed)
+})}}>Змінити</p>
+
       </div>
     </section>
   )
