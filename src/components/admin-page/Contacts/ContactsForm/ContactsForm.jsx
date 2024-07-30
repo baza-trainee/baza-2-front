55175c370e6feb73075422e2e5ab9961eff9e4f4
artyomakulov <artyomakulov@gmail.com>
2024-07-30 20:17:10 +0300

    add Inputs

--- src/components/admin-page/Contacts/ContactsForm/ContactsForm.jsx
+++ src/components/admin-page/Contacts/ContactsForm/ContactsForm.jsx
@@ -1,5 +1,73 @@
 import InputField from "@/src/components/shared/InputField/InputField";
+import styles from "./ContactsForm.module.scss";
 
 export default function ContactsForm() {
-  return <>wtf</>;
+  return (
+    <div className={styles.form}>
+      <div className={styles.row}>
+        <div className={styles.input}>
+          <InputField
+            id="phone1"
+            required={false}
+            type="textArea"
+            placeholder="Введіть телефон"
+            version="input_admin"
+            label="Телефон"
+          />
+        </div>
+        <div className={styles.input}>
+          <InputField
+            id="phone2"
+            required={false}
+            type="textArea"
+            placeholder="Введіть телефон"
+            version="input_admin"
+            label="Телефон"
+          />
+        </div>
+        <div className={styles.input}>
+          <InputField
+            id="email"
+            required={false}
+            type="textArea"
+            placeholder="Введіть електронну пошту"
+            version="input_admin"
+            label="Email"
+          />
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.input}>
+          <InputField
+            id="telegram"
+            required={false}
+            type="textArea"
+            placeholder="Додайте посилання"
+            version="input_admin"
+            label="Telegram"
+          />
+        </div>
+        <div className={styles.input}>
+          <InputField
+            id="facebook"
+            required={false}
+            type="textArea"
+            placeholder="Додайте посилання"
+            version="input_admin"
+            label="Facebook"
+          />
+        </div>
+        <div className={styles.input}>
+          <InputField
+            id="linkedin"
+            required={false}
+            type="textArea"
+            placeholder="Додайте посилання"
+            version="input_admin"
+            label="Linkedin"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
