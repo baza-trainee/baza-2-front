f8f9dcac2284140d0dc28ebbe441fdbda36d03eb
Богдан <sysoevbogdan82@gmail.com>
2024-07-31 15:27:13 +0300

    fix: fixed fetching projects

--- src/app/[locale]/projects/page.jsx
+++ src/app/[locale]/projects/page.jsx
@@ -1,11 +1,11 @@
 import SorryModal from "@/src/components/modals/SorryModal/SorryModal";
-import Projects from "@/src/components/projects-page/Projects/Projects";
+import Projects from "@/src/components/projects-page/Projects";
 
 export default function ProjectsPage() {
   return (
     <main>
       <Projects />
-      <SorryModal/>
+      <SorryModal />
     </main>
   );
 }
