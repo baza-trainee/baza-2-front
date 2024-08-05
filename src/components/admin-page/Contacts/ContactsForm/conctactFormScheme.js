acb449683d5aa1d50d4a51082238a8d7b48c3448
artyomakulov <artyomakulov@gmail.com>
2024-08-05 17:55:49 +0200

    updated code

--- src/components/admin-page/Contacts/ContactsForm/conctactFormScheme.js
+++ src/components/admin-page/Contacts/ContactsForm/conctactFormScheme.js
@@ -4,26 +4,10 @@ import {
   patternLink,
   patternPhone,
 } from "@/src/constants/regulars";
-import { formatPhoneNumber } from "@/src/lib/utils/formatPhoneNumber";
 import { z } from "zod";
 
-export const contactsDefaultValues = {
-  phone1: "",
-  phone2: "",
-  email: "",
-  telegram: "",
-  facebook: "",
-  linkedin: "",
-};
-
-export const contactsScheme = z.object({
-  phone1: z
-    .string()
-    .trim()
-    .regex(patternPhone, { message: "incorrect_phone" })
-    .transform((value) => formatPhoneNumber(value, true)),
-
-  phone2: z
+export const ContactsScheme = z.object({
+  phone: z
     .string()
     .trim()
     .regex(patternPhone, { message: "incorrect_phone" })
@@ -35,17 +19,7 @@ export const contactsScheme = z.object({
     .regex(patternEmail, { message: "incorrect_email" })
     .regex(patternEmailNonRu, { message: "invalid_ru" }),
 
-  telegram: z
-    .string()
-    .trim()
-    .regex(patternLink, { message: "Введіть дійсний URL" }),
-
-  facebook: z
-    .string()
-    .trim()
-    .regex(patternLink, { message: "Введіть дійсний URL" }),
-
-  linkedin: z
+  validUrl: z
     .string()
     .trim()
     .regex(patternLink, { message: "Введіть дійсний URL" }),
