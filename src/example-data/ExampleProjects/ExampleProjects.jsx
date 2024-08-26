330d04f89d745e69f1cd533c790fd97132e5cbc1
Brodich Vlad <brodich_vlad@ukr.net>
2024-08-26 10:25:08 +0300

    add ExampleProjects

--- src/example-data/ExampleProjects/ExampleProjects.jsx
+++ src/example-data/ExampleProjects/ExampleProjects.jsx
@@ -1,115 +1,99 @@
 "use client";
 
-//import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import styles from "./ExampleProjects.module.scss";
 import { getAllProjects } from "@/src/api/projects";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { createImageUrlBaza1 } from "@/src/lib/hooks/createImageUrl";
 import { createKey } from "@/src/lib/utils/createKey";
 import MainButton from "@/src/components/shared/MainButton/MainButton";
 import InputSearch from "@/src/components/shared/inputs/InputSearch/InputSearch";
 import Loader from "@/src/components/shared/loader/Loader";
 import ProjectCard from "@/src/components/projects-page/ProjectCard/ProjectCard";
-import { QueryClient, useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
-//import React, { useState } from 'react';
-//import { useInfiniteQuery } from 'react-query';
-//import { getAllProjects } from './api'; // Імпортуємо вашу функцію
-
+import stateSorryModal from "@/src/state/stateSorryModal";
+import SorryModal from "@/src/components/modals/SorryModal/SorryModal";
 
 export default function ExampleProjects() {
   const [searchQuery, setSearchQuery] = useState(''); // Стан для пошуку
-  const [limit, setLimit] = useState(6); // Обмеження кількості елементів на сторінку
-  const [queryKey, setQueryKey] = useState(['projects', '']);
-  // const queryClient = useQueryClient(); // Ініціалізуємо queryClient для керування кешем
 
+  const queryClient = useQueryClient(); // Ініціалізуємо queryClient для керування кешем
+  const open = stateSorryModal(state => state.open);// Керування модалкою
+
+// Функція useInfiniteQuery нескінченних запитів
   const {
     data,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
-    status,
-    refetch,
-    reset,
+    isError,
+    isLoading,
   } = useInfiniteQuery({
-    queryKey: queryKey, // Передаємо ключ запиту та пошук
-    queryFn: ({ pageParam = 1 }) => getAllProjects({ page: pageParam, search: searchQuery, limit }), // Використовуємо вашу функцію з параметрами
+    queryKey: ['projects', searchQuery ], // Передаємо ключ запиту та пошук
+
+    queryFn: ({ pageParam = 1 }) => getAllProjects({ page: pageParam, search: searchQuery,limit:6 }), // Використовуємо функцію з параметрами
+
     getNextPageParam: (lastPage, allPages) => {
       // Логіка для визначення наступної сторінки
-    
-      const morePagesExist = lastPage?.results?.length === limit;
-    
-       // Якщо кількість проектів дорівнює ліміту, значить є ще сторінки
+      const morePagesExist = lastPage?.pagination.currentPage < lastPage?.pagination.totalPages 
       return morePagesExist ? allPages.length + 1 : undefined; 
     },
-    keepPreviousData: false,
-    //enabled: false, // Автоматичне завантаження вимкнене
   });
 
-  // const handleSearch = () => {
-  //   refetch(); // Оновлюємо запит при натисканні на кнопку
-  // };
-
-  const handleSearchChange = (value) => {
+ // Управління пошуком
+  const handleSearchChange = (value='') => {
     setSearchQuery(value);
-    setQueryKey(['projects', value]);
-    data.pages.length = 1
-    refetch(); 
-    //data.pages.length
     // Інвалідовуємо запит, щоб очистити кешовані сторінки
-    // queryClient.invalidateQueries(['projects',searchQuery]);
-
-    // refetch()
-    // Якщо поле очищене, скидаємо всі дані і перезапускаємо запит
-    // if (value === '') {
-    //   //queryClient.invalidateQueries({ queryKey: ["projects"] })
-    //   //reset(); // Очищуємо кеш запиту
-    //   refetch(); // Повторно завантажуємо дані з першої сторінки
-    // } else {
-    //   refetch(); // Виконуємо пошук з новим запитом
-    // }
+    if (value === '') {
+      queryClient.resetQueries(['projects'], { exact: true });
+    }
   };
+
+// Як що нічого не знайдено відкриваємо модалку
+  useEffect(()=>{
+    if(data?.pages[0].results.length === 0){
+      open()
+    }
+  },[data])
+
 
   return (
     <div className={styles.wrapper}>
       {/* Поле для введення пошукового запиту */}
-      {/* <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Пошук проектів..."
-      />
-      <button onClick={handleSearch}>Знайти</button> */}
-      <InputSearch onSubmit={handleSearchChange} />
+      <InputSearch onSubmit={handleSearchChange} defaultValue={searchQuery}/>
+
       {/* Обробка статусів */}
-      {status === 'loading' && <Loader />}
-      {status === 'error' && <div>Сталася помилка</div>}
+      {isLoading && <Loader />}
+      {isError && <p>Сталася помилка</p>}
 
       {/* Відображення проектів */}
-      {data?.pages.map((page, i) => (
-  
-        <div key={i} className={styles.content}>
-
-          {page.results.map((el) => (
-  
+      <div className={styles.content_wrapper}>
+         {/*data.pages - завантажені сторінки*/}
+        {data?.pages.map((page, i) => (
+    
+          <div key={i} className={styles.content}>
+            {/* page.results - кожна окрема сторінка*/}
+            {page.results.map((el) => (
               <ProjectCard
                 project={el}
                 coverImgUrl={createImageUrlBaza1(el.imageUrl)}
                 key={createKey()}
               />
-          ))}
-        </div>
-      ))}
-
-      {/* Кнопка для завантаження більше проектів */}
-      {hasNextPage && (
-        <MainButton
-        onClick={() => fetchNextPage()}
-        disabled={isFetchingNextPage}
-        >
-         {isFetchingNextPage ? 'Завантаження...' : 'Завантажити ще'}
-        </MainButton>
-      )}
+            ))}
+          </div>
+        ))}
+    
+        {/* Кнопка для завантаження більше проектів hasNextPage- чи є наступна сторінка*/}
+        {hasNextPage && (
+          <MainButton
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+          >
+          {'Більше проектів'}
+          </MainButton>
+        )}
+      </div>
+      {/* Модалка - як що нічого не знайдено */}
+      <SorryModal handleCallback={handleSearchChange}/>
     </div>
   );
 };
