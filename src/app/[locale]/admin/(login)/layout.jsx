b53dbc9239212bac9e960da0af538b8f508733a0
Brodich-vlad <114187879+Brodich-vlad@users.noreply.github.com>
2024-08-31 17:30:40 +0300

    Feature/refactoring code (#292)
    * fix close SorryModal
    
    * fix PaymentModal
    
    * fix checkFileType
    
    * add env.example
    
    * fix login/page.
    
    * add robots
    
    * fix robots
    
    * fix sitemap

--- src/app/[locale]/admin/(login)/layout.jsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: "Baza Trainee 2 Login",
-  description: "Baza Trainee page Login",
-};
-
-export default function layoutLogin( {children}) {
-  return <>{children}</>
-};
