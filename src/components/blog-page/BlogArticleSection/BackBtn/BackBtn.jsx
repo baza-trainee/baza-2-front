202ce8c778772d2411d7e387d0dc671ee2df9149
Sysoev Bogdan <112561940+Lundoger@users.noreply.github.com>
2024-09-08 14:19:06 +0300

    Feature/blog page (#315)
    * feat: added translition, connected to admin.
    
    * Update BlogSection.module.scss
    
    * feat: added button instead link for admin page
    
    * fix: minor fixes and updates for blog
    
    * fix: minor fixes for blog page
    
    * Update LoadMore.module.scss
    
    * feat: connected blog article to admin and fixes few errors with markup
    
    * Update BlogList.jsx
    
    * fix: minor fixes for blog article

--- src/components/blog-page/BlogArticleSection/BackBtn/BackBtn.jsx
+++ src/components/blog-page/BlogArticleSection/BackBtn/BackBtn.jsx
@@ -3,14 +3,16 @@
 import { useRouter } from "@/src/navigation";
 import styles from "./BackBtn.module.scss";
 import { Icon } from "../../../shared/Icon/Icon";
+import { useTranslations } from "next-intl";
 
 const BackBtn = () => {
+  const t = useTranslations("Blog");
   const router = useRouter();
 
   return (
     <button type="button" onClick={() => router.back()} className={styles.btn}>
       <Icon name="carousel-arrow" className={styles.goBackIcon} />
-      <span>Повернутись до статей</span>
+      <span>{t("back_btn")}</span>
     </button>
   );
 };
