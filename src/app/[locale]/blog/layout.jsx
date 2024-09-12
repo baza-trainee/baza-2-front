66f862c5010ab0663c137127b0c3e5af1107fa0f
Brodich-vlad <114187879+Brodich-vlad@users.noreply.github.com>
2024-09-12 14:03:32 +0300

    Feature/seo optimization (#330)
    * Update hreflang layout
    
    * delete comments
    
    * add alternates to projects internship blog pages
    
    * fix sitemap
    
    * fix sitemap
    
    * add canonicalUrl
    
    * fix layout
    
    * add metadata to  messages.json
    
    * add metadata to article
    
    * fix BlogArticleForm
    
    ---------
    
    Co-authored-by: Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>

--- src/app/[locale]/blog/layout.jsx
+++ src/app/[locale]/blog/layout.jsx
@@ -1,24 +1,15 @@
-// metadata
-const metadataBlogPage ={ 
-  ua:{ 
-    title: "Блог: тільки корисне і цікаве про розробку і практику", 
-    description : "Baza Trainee Ukraine Блог: Підпишись на останні новини про старт проєктів і не пропусти унікальну можливість долучитись" 
-  }, 
-  en:{ 
-    title: "Blog: only useful and fresh themes about development and practice", 
-    description : "Baza Trainee Ukraine Blog: Sign up to receive the latest news about the projects launch and don't miss this unique opportunity to get involved " 
-  }, 
-  pl:{ 
-    title: "Blog: tylko i wyłącznie przydatne informacje na temat rozwoju i praktyki", 
-    description : "Baza Trainee Ukraine Blog: Zapisz się, aby otrzymywać najnowsze informacje o start projektu i nie przegap tej wyjątkowej okazji, aby się zaangażować" 
-  }, 
-}
+import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata({ params }){
+  const t = await getTranslations({
+    locale:params.locale, 
+    namespace: 'Metadata'
+  });
+
   const canonicalUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/${params.locale}/blog`;
   return {
-    title: metadataBlogPage[params.locale].title,
-    description: metadataBlogPage[params.locale].description,
+    title: t('blog_title'),
+    description: t('blog_description'),
     alternates: {
       canonical: canonicalUrl,
     },
