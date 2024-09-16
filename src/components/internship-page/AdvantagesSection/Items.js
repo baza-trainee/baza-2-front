696dff7ffc6756c375bde9b25aac0b1397b80524
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2024-09-16 08:14:48 +0300

    add text, fix code (#338)

--- src/components/internship-page/AdvantagesSection/Items.js
+++ src/components/internship-page/AdvantagesSection/Items.js
@@ -2,25 +2,31 @@ export const items = [
   {
     img: "/images/advantages/1.png",
     text: "item_1",
+    altText: "alt_1",
   },
   {
     img: "/images/advantages/2.png",
     text: "item_2",
+    altText: "alt_2",
   },
   {
     img: "/images/advantages/3.png",
     text: "item_3",
+    altText: "alt_3",
   },
   {
     img: "/images/advantages/4.png",
     text: "item_4",
+    altText: "alt_4",
   },
   {
     img: "/images/advantages/5.png",
     text: "item_5",
+    altText: "alt_5",
   },
   {
     img: "/images/advantages/6.png",
     text: "item_6",
+    altText: "alt_6",
   },
 ];
