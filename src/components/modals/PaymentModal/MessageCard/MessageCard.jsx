a96874ec17c5ac01fe09e43086ab8c626051c69f
Brodich Vlad <brodich_vlad@ukr.net>
2024-09-17 12:52:36 +0300

    fix PaymentModal

--- src/components/modals/PaymentModal/MessageCard/MessageCard.jsx
+++ src/components/modals/PaymentModal/MessageCard/MessageCard.jsx
@@ -1,15 +1,9 @@
 import { useTranslations } from 'next-intl';
 import styles from './MessageCard.module.scss';
 import ButtonLink from '@/src/components/shared/ButtonLink/ButtonLink';
-import { isMobile } from 'react-device-detect';
 
 export default function MessageCard({handleClose, isError, isThanks}){
   const t = useTranslations("Modal_support");
-
-  if(isMobile && !isError){
-    handleClose()
-    return null
-  }
 
   return <div className={styles.thanks}>
     {isThanks && <h2>{t('thanks_support')}</h2>}
