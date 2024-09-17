f09f8ed76803a3cb38abd403d6e1023d2f507956
artyomakulov <artyomakulov@gmail.com>
2024-09-17 10:31:31 -0500

    Update FeedbackCard.jsx

--- src/components/shared/FeedbackCard/FeedbackCard.jsx
+++ src/components/shared/FeedbackCard/FeedbackCard.jsx
@@ -4,23 +4,38 @@ import { createImageUrl } from "@/src/lib/hooks/createImageUrl";
 import clsx from "clsx";
 import { formatDate } from "@/src/lib/utils/formatData";
 
-const FeedbackCard = ({ imageUrl, name, role, date, review, className, locale}) => {
+const FeedbackCard = ({
+  imageUrl,
+  name,
+  role,
+  date,
+  review,
+  className,
+  locale,
+}) => {
   return (
     <div className={clsx(styles.card, className)}>
       <div className={styles.person}>
         <div className={styles.imageContainer}>
-          <Image fill sizes="100%" src={createImageUrl(imageUrl)} alt={name[locale]} />
+          <Image
+            fill
+            sizes="100%"
+            src={createImageUrl(imageUrl)}
+            alt={name[localeUkToUa(locale)]}
+          />
         </div>
         <div className={styles.profile}>
           <div className={styles.personInfo}>
-            <p className={styles.name}>{name[locale]}</p>
+            <p className={styles.name}>{name[localeUkToUa(locale)]}</p>
             <p className={styles.role}>{role}</p>
-            <p className={styles.date}>{formatDate(date, locale,'feedback')}</p>
+            <p className={styles.date}>
+              {formatDate(date, locale, "feedback")}
+            </p>
           </div>
         </div>
       </div>
       <div>
-        <p className={styles.text}>“{review[locale]}”</p>
+        <p className={styles.text}>“{review[localeUkToUa(locale)]}”</p>
       </div>
     </div>
   );
