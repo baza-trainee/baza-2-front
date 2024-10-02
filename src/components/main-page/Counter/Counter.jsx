235b8b9f8e14664bddea70f08483ef3e99c85c67
Богдан <sysoevbogdan82@gmail.com>
2024-10-02 16:16:07 +0300

    fix: fixed counter animation

--- src/components/main-page/Counter/Counter.jsx
+++ src/components/main-page/Counter/Counter.jsx
@@ -5,41 +5,46 @@ import { useTranslations } from "next-intl";
 import { createKey } from "@/src/lib/utils/createKey";
 import { useQuery } from "@tanstack/react-query";
 import { getData } from "@/src/api/achievements";
+import { useInView } from "react-intersection-observer";
 
 const CounterComponent = () => {
   const t = useTranslations("Main.counter_section");
   // Функція запиту
   const employed = useQuery({ queryKey: ["employed"], queryFn: getData });
-
   const data = [
     {
       id: 1,
-      count: employed.data?.projects ? employed.data.projects : "41",
+      count: employed.data?.projects ? employed.data.projects : "46",
       title: "active_projects",
     },
     {
       id: 2,
-      count: employed.data?.members ? employed.data.members : "371",
+      count: employed.data?.members ? employed.data.members : "417",
       title: "involved_participants",
     },
     {
       id: 3,
-      count: employed.data?.employed ? employed.data.employed : "200",
+      count: employed.data?.employed ? employed.data.employed : "110",
       title: "employed",
     },
   ];
 
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+
   return (
     <section className={styles.counter}>
-      <div className={styles.container}>
+      <div ref={ref} className={styles.container}>
         {data.map((item) => {
           return (
             <div className={styles.item} key={createKey()}>
               <span className={styles.numbers}>
                 <CountUp
                   start={0}
-                  end={item.count}
-                  duration={2}
+                  end={inView ? item.count : 0}
+                  duration={3}
                   formattingFn={(value) => `${value}+`}
                 />
               </span>
