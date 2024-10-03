c2e5e573eb50bb195fa48981802d7e708a34ce28
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-03 12:45:19 +0300

    fix FormaterBlogTex

--- src/components/shared/FormaterBlogText/FormaterBlogText.jsx
+++ src/components/shared/FormaterBlogText/FormaterBlogText.jsx
@@ -3,13 +3,11 @@ import { createKey } from "@/src/lib/utils/createKey";
 
 export default function FormaterBlogText(text, className){
   const rows = text.split(/\r\n|\r|\n/g);
-  console.log(rows)
 
   return (
     <>
       {
         rows.map((el)=>{
-          console.log(el.length)
           if(el.length==1){
             return <div key={createKey()} className={styles.paragraph}>{el}</div>
           }else{ 
