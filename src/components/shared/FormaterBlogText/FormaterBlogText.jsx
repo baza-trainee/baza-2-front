ba9e382634eaa8c89b8354d2fdb101c7aca75887
Brodich Vlad <brodich_vlad@ukr.net>
2024-10-03 12:42:40 +0300

    add FormaterBlogText

--- src/components/shared/FormaterBlogText/FormaterBlogText.jsx
+++ src/components/shared/FormaterBlogText/FormaterBlogText.jsx
@@ -1,15 +1,23 @@
+import styles from './FormaterBlogText.module.scss'
 import { createKey } from "@/src/lib/utils/createKey";
 
 export default function FormaterBlogText(text, className){
   const rows = text.split(/\r\n|\r|\n/g);
+  console.log(rows)
 
   return (
     <>
       {
         rows.map((el)=>{
-          if(el.length){
+          console.log(el.length)
+          if(el.length==1){
+            return <div key={createKey()} className={styles.paragraph}>{el}</div>
+          }else{ 
             return <p key={createKey()} className={className}>{el}</p>
-          }else return null
+          }
+          // if(el.length){
+          //   return <p key={createKey()} className={className}>{el}</p>
+          // }else return <div key={createKey()} className={styles.paragraph}></div>
         })
       }
     </>
